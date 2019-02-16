8817fe1ffafe7561c93815a94a7c4e662c622402
Dmitry <hacker.dmitry@gmail.com>
2019-02-16 22:07:37 +0500

    Solving task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -1,21 +1,14 @@
 package com.example.task05;
 
 public class Task05 {
-
-    public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+    static String solution(int x) {
+        while (x % 2 == 0 && x != 0)
+            x /= 10;
+        return x == 0 ? "TRUE" : "FALSE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
+        String result = solution(24680248);
         System.out.println(result);
-        */
     }
-
 }
