aa0a9d945ac285a2a952cc0e60732bbe1e16df39
Dmitry <hacker.dmitry@gmail.com>
2019-02-16 22:00:57 +0500

    Please, build

--- task01/src/com/example/task01/Task01.java
+++ task01/src/com/example/task01/Task01.java
@@ -1,12 +1,14 @@
 package com.example.task01;
 
-public class Task01 {
+class Task01 {
     static String solution() {
         return "Я думаю, быть программистом - это круто";
     }
 
+    /*
     public static void main(String[] args) {
         String result = solution();
         System.out.println(result);
     }
+    */
 }
