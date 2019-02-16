5888f5264ba30eb8c7ba2eb5267794d27c9ca603
Dmitry <hacker.dmitry@gmail.com>
2019-02-17 00:09:47 +0500

    Solving task10

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -1,15 +1,12 @@
 package com.example.task10;
 
 public class Task10 {
-
-    public static boolean compare(float a, float b, int precision) {
-
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
-
+    static boolean compare(float a, float b, int precision) {
+        float epsilon = 1f;
+        for (int i = 0; i < precision; i++) {
+            epsilon *= 0.1;
+        }
+        return Math.abs(a - b) < epsilon || Double.isNaN(a) && Double.isNaN(b) || a == b;
     }
 
     public static void main(String[] args) {
@@ -20,7 +17,5 @@ public class Task10 {
 
         boolean result = compare(sum, c, 2);
         System.out.println(result);
-
     }
-
 }
