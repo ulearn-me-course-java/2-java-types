07d63221875563f1a0cab9271ad2ac885e51702f
Dmitry <hacker.dmitry@gmail.com>
2019-02-16 22:12:19 +0500

    Solving task06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -1,21 +1,12 @@
 package com.example.task06;
 
 public class Task06 {
-
-    public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+    static int solution(int x, int y) {
+        return String.valueOf(Math.abs(x + y)).length();
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
+        int result = solution(-10, 10);
         System.out.println(result);
-        */
     }
-
 }
