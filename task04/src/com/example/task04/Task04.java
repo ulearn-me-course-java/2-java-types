4b51050e20200cdf2adbc17d50952baca676d024
Dmitry <hacker.dmitry@gmail.com>
2019-02-16 21:56:51 +0500

    Implemented method "calculate"

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -1,21 +1,23 @@
 package com.example.task04;
 
 public class Task04 {
-
-    public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+    static float calculate(int a, int b, String operation) {
+        switch (operation) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "/":
+                return (float)a / b;
+            case "*":
+                return a * b;
+        }
+        throw new IllegalArgumentException("Данная операция выходит за пределы знания текущего метода");
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        float result = calculate(-25, 5, "/");
+        float result = calculate(2, 2, "+");
         System.out.println(result);
-        */
     }
 
 }
