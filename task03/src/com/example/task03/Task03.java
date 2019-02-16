2e7fdf36699194247da0626ec02569a8be28dea8
Dmitry <hacker.dmitry@gmail.com>
2019-02-16 21:44:00 +0500

    Implemented method "getMetreFromCentimetre"

--- task03/src/com/example/task03/Task03.java
+++ task03/src/com/example/task03/Task03.java
@@ -1,21 +1,12 @@
 package com.example.task03;
 
 public class Task03 {
-
-    public static int getMetreFromCentimetre(int centimetre) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+    static int getMetreFromCentimetre(int centimetre) {
+        return centimetre / 100;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = getMetreFromCentimetre(345);
+        int result = getMetreFromCentimetre(73);
         System.out.println(result);
-         */
     }
-
 }
