8021dc17a6d2015cea1eec0ec9b8867d81b62e62
Henkka <henkkawar@gmail.com>
2019-02-16 20:46:53 +0500

    first task

--- task01/src/com/example/task01/Task01.java
+++ task01/src/com/example/task01/Task01.java
@@ -2,11 +2,8 @@ package com.example.task01;
 
 public class Task01 {
 
-    public static String solution() {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "здесь какая-то неправильная строка";
+    static String solution() {
+        return "Я думаю, быть программистом - это круто";
     }
 
     public static void main(String[] args) {
