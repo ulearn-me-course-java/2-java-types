b430fb685cfa313388f2a05083fb2c5c52befc5a
Henkka <henkkawar@gmail.com>
2019-02-16 22:03:31 +0500

    second task

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -1,21 +1,30 @@
 package com.example.task02;
 
+
 public class Task02 {
 
-    public static String solution(String input) {
+    // а что если input больше long?
+    // К тестам можно добавить NumberFormatException (или оборачивать в BigInteger...но задание того не требует)
+    static String solution(String input) {
+        return solution(Long.parseLong(input));
+    }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+    static String solution(long input) {
+        return isBetween(input, Byte.MAX_VALUE, Byte.MIN_VALUE) ? "byte" :
+                isBetween(input, Short.MAX_VALUE, Short.MIN_VALUE) ? "short" :
+                        isBetween(input, Integer.MAX_VALUE, Integer.MIN_VALUE) ? "int" : "long";
+    }
 
-        return "";
+    static boolean isBetween(long value, long from, long to) {
+        return value <= from && value >= to;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
         /*
-        String result = solution("12345");
+        String result = solution("1000000000000000000000000000000000000000000000000000000005");
         System.out.println(result);
-         */
+        */
     }
-
 }
