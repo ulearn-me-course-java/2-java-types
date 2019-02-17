d7211c57bab2352dec2254f3a62d23c9fcb8cdbb
wisedog706 <psnetwork1138@gmail.com>
2019-02-17 22:32:56 +0500

    best percent v2

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -5,19 +5,12 @@ import java.math.BigDecimal;
 public class Task12 {
 
     public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
-
-        // TODO раскомментируйте и исправьте код
-
         // Считаем проценты за год
-
-        /*
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+            sum = sum.add(sum.multiply(percent));
         }
-        return sum;
-        */
 
-        return BigDecimal.ZERO;
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
     }
 
     public static void main(String[] args) {
@@ -28,7 +21,6 @@ public class Task12 {
         sum = benefit(sum, percent);
 
         System.out.println("Сумма на счете через год: " + sum);
-
     }
 
 }
