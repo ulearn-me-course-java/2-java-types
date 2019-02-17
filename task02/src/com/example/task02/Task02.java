7ee59ab06e89d595a75676ca54130ff20dc37eec
Дмитрий <Mystache@yandex.ru>
2019-02-17 10:36:07 +0500

    Solution #2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -1,21 +1,18 @@
 package com.example.task02;
 
 public class Task02 {
-
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long convertInput = Long.valueOf(input);
+        if (convertInput == (byte)convertInput)
+            return "byte";
+        if (convertInput == (short)convertInput)
+            return "short";
+        if (convertInput == (int)convertInput)
+            return "int";
+        return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+        System.out.println(solution("1412253413223"));
     }
-
 }
