b89498819cda2a254cb6dd3f99c097c65d4493a2
wisedog706 <psnetwork1138@gmail.com>
2019-02-19 12:40:38 +0500

    Task1

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -1,21 +1,35 @@
 package com.example.task02;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 public class Task02 {
+    private static long max = 0;
 
     public static String solution(String input) {
+        if (!tryParseLong(input))
+            if (input.toCharArray().length == 1) return "char";
+            else return "its no integral type";
+        else {
+            if (max == (byte) max) return "byte";
+            if (max == (short) max) return "short";
+            if (max == (int) max) return "int";
+            else return "long";
+        }
+    }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+    private static boolean tryParseLong(String value) {
+        try {
+            max = Long.parseLong(value);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+        System.out.println(solution("9"));
+        System.out.println(solution("E"));
+        System.out.println(solution("string"));
     }
 
 }
