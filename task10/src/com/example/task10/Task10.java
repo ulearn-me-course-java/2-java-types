b374e951f10b565c48cf5250fa2a6649f797dacc
wisedog706 <psnetwork1138@gmail.com>
2019-02-19 18:14:39 +0500

    Task10

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -4,18 +4,17 @@ import java.nio.channels.FileLock;
 
 public class Task10 {
 
-    public static boolean compare(float a, float b, int precision) {
+    static boolean compare(float a, float b, int precision) {
         if (Float.isNaN(a) && Float.isNaN(b)) return true;
         else if (Float.isInfinite(a) || Float.isInfinite(b)) return a == b;
         else return (Math.abs(a-b) < Math.pow(10, -precision));
     }
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         float a = 0.3f;
         float b = 0.4f;
         float sum = a + b;
         float c = 0.7f;
-
         boolean result = compare(sum, c, 2);
         System.out.println(result);
     }
