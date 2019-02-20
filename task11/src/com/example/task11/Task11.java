dec019830cd802453201a85ad8cc75d9ce388685
Pavel <pavel.zaborin@inbox.ru>
2019-02-20 19:42:08 +0500

    Заборин Павел. Задание 11

--- task11/src/com/example/task11/Task11.java
+++ task11/src/com/example/task11/Task11.java
@@ -3,14 +3,7 @@ package com.example.task11;
 public class Task11 {
 
     public static float benefit(float sum, float percent) {
-
-        // TODO исправьте функцию, чтобы избежать накопления ошибки
-
-        // Считаем проценты за год
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
+        return (float)(sum * Math.pow(percent + 1.0, 12));
     }
 
     public static void main(String[] args) {
@@ -19,7 +12,6 @@ public class Task11 {
         float percent = 0.00000001f; // 0.000001% ежемесячно
 
         sum = benefit(sum, percent);
-
         System.out.println("Сумма на счете через год: " + sum);
 
     }
