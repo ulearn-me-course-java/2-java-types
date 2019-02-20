9b1f2b9b3dda4844a26e74d1fb5055bec978780a
Алексей Ступников <stuq11@gmail.com>
2019-02-20 23:32:15 +0500

    Задача 2 выполнена

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,22 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long parseInput = Long.valueOf(input);
+		
+        if (parseInput == (byte)parseInput)
+            return "byte";
+		
+        if (parseInput == (short)parseInput)
+            return "short";
+		
+        if (parseInput == (int)parseInput)
+            return "int";
+		
+        return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+		
     }
 
 }
