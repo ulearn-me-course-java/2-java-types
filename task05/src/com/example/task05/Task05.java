49fdb86dcbc186105e2ff085ae478cdfe93c4534
Dmitriy <Mystache@yandex.ru>
2019-02-20 20:51:28 +0500

    Solution #5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -1,21 +1,15 @@
 package com.example.task05;
 
 public class Task05 {
-
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        char[] array = String.valueOf(x).toCharArray();
+        for (int i = 0; i < array.length; i++)
+            if (array[i] % 2 != 0)
+                return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
+        System.out.println(solution(42680));
     }
-
 }
