66accd2724f5c843c5bad4032c835d35911c99c3
Platonov2 <jek1999060@mail.ru>
2019-02-20 15:37:10 +0500

    Платонов Евгений. Задание 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,18 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        long temp = Math.abs(Long.parseLong(input)) - 1;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if (temp > Integer.MAX_VALUE) return "long";
+        if (temp > Short.MAX_VALUE) return "int";
+        if (temp > Byte.MAX_VALUE) return "short";
 
-        return "";
+        return "byte";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
+        String result = solution("-2147483648");
         System.out.println(result);
-         */
     }
 
 }
