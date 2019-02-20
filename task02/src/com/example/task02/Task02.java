b356254066157a1ad24579c12aba53420cb37df7
Platonov2 <jek1999060@mail.ru>
2019-02-20 16:24:32 +0500

    Платонов Евгений. Задание 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,7 +4,7 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        long temp = Math.abs(Long.parseLong(input));
+        long temp = Math.abs(Long.parseLong(input)) - 1;
 
         if (temp > Integer.MAX_VALUE) return "long";
         if (temp > Short.MAX_VALUE) return "int";
@@ -14,12 +14,10 @@ public class Task02 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution("12345");
         System.out.println(result);
-         */
+
     }
 
 }
