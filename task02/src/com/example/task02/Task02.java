1433f83325bd954c6d0a5cdc4bb8c7dceedc9612
Макс <uue33@mail.ru>
2019-02-20 15:15:21 +0500

    Иконникова Анастасия. Задание 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,24 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        String output = "Input error.";
+        try {
+            Long.parseLong(input);//64 bit
+            output = "long";
+            Integer.parseInt(input);//32 bit
+            output = "int";
+            Short.parseShort(input);//16 bit
+            output = "short";
+            Byte.parseByte(input);//8 bit
+            return "byte";
+        } catch (Exception e) {
+            return output;
+        }
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+        /*String result = solution("12345");
+        System.out.println(result);*/
     }
 
 }
