bde30d49b12c074813fb175f5a33213b6e14b419
student <psnetwork1138@gmail.com>
2019-02-21 12:18:49 +0500

    Task2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,24 +4,15 @@ public class Task02 {
     private static long max = 0;
 
     public static String solution(String input) {
-        if (!tryParseLong(input))
-            if (input.toCharArray().length == 1) return "char";
-            else return "its no integral type";
-        else {
+        try {
+            max = Long.parseLong(input);
             if (max == (byte) max) return "byte";
             if (max == (short) max) return "short";
             if (max == (int) max) return "int";
             else return "long";
-        }
-    }
 
-    private static boolean tryParseLong(String value) {
-        try {
-            max = Long.parseLong(value);
-            return true;
-        } catch (Exception ex) {
-            return false;
         }
+        catch (Exception ex) {return "No Integral Type";}
     }
 
     public static void main(String[] args) {
