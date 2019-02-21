f2362c84921553de2c7b1e73849a1e223dc548d4
AllaOsipyan <allosipyan@mail.ru>
2019-02-21 18:02:31 +0500

    Осипян Пайцар. Задание 05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,10 +3,12 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        for (int i = 1; i <= 5; i++) {
+            int digit = (x % (int) Math.pow(10, i)) / (int) Math.pow(10, i - 1);
+            if (digit % 2 != 0)
+                return "FALSE";
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
