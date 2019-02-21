693cc9eb329a71c1b2e26f0356cfc50e13316036
uytera <uytera12@yandex.ru>
2019-02-21 20:29:17 +0500

    Даниил Бакулин. Задание 05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,18 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        int y;
+        while(x != 0)
+        {
+            y = x%10;
+            x = x/10;
+            if(y % 2 != 0)
+                return "FALSE";
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
+        String result = solution(17395);
     }
-
 }
