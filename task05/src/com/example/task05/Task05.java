ade0651438a3e315956b548ae74285e54b256aae
PotapovDmitiriy <lhfujy7477@gmail.com>
2019-02-21 12:32:20 +0500

    Потапов Дмитрий. Задание 5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,21 +3,17 @@ package com.example.task05;
 public class Task05 {
 
     static String solution(int x) {
-        String strX = String.valueOf(x);
         boolean t = true;
-        for (int i = 0; i < strX.length(); i++) {
-            if ((strX.toCharArray()[i] % 2) == 1) {
-                t = false;
-                break;
-            }
+        while (x != 0) {
+            if ((x % 10) % 2 != 0)
+                return "FALSE";
+            x = x / 10;
         }
-        if (t)
-            return "TRUE";
-        else return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        String result = solution(88889);
+        String result = solution(8888);
         System.out.println(result);
     }
 
