349fd39e0db976bcadcd8c48376595e05579b6a4
NewGameStudio <andre-orsk@yandex.ru>
2019-02-22 10:15:17 +0500

    Update Task04.java

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -4,18 +4,24 @@ public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        float num1 = (float) a;
+        float num2 = (float) b;
 
-        return 0;
+        if (operation.equals("+"))
+            num1 += num2;
+        else if (operation.equals("-"))
+            num1 -= num2;
+        else if (operation.equals("*"))
+            num1 *= num2;
+        else if (operation.equals("/"))
+            num1 /= num2;
+
+        return num1;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        float result = calculate(-25, 5, "/");
-        System.out.println(result);
-        */
+        System.out.println(calculate(10_000_000, 0, "/"));
+
     }
 
 }
