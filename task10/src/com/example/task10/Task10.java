5d03fafa9db5e518fd62df8cb81e05eb93e0cf64
NewGameStudio <andre-orsk@yandex.ru>
2019-02-22 10:16:45 +0500

    Update Task10.java

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -4,12 +4,7 @@ public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
 
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
-
+        return (Float.compare(a, b) == 0) || (Math.abs(a - b) < Math.pow(10, -precision));
     }
 
     public static void main(String[] args) {
@@ -20,7 +15,6 @@ public class Task10 {
 
         boolean result = compare(sum, c, 2);
         System.out.println(result);
-
     }
 
 }
