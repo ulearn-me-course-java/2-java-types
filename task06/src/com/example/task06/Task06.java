1248a9dd1a0e0b043e66ce43dac715df21138af1
NewGameStudio <andre-orsk@yandex.ru>
2019-02-22 10:15:32 +0500

    Update Task06.java

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,23 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int sum = x + y;
 
-        return 0;
+        if(sum == 0)
+            return 1;
+
+        int count = 0;
+        while (sum != 0)
+        {
+            sum /= 10;
+            count++;
+        }
+
+        return count;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
-        System.out.println(result);
-        */
+        System.out.println(solution(12, 34));
     }
 
 }
