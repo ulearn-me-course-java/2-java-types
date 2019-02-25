0cdefea3a38ab9b68a3ffcc03eb4119e725fbb1f
Саша <gorgonova@bk.ru>
2019-02-25 21:33:07 +0500

    Musina task10

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -4,11 +4,11 @@ public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
 
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
+        if (Float.isNaN(a) && Float.isNaN(b)) return true;
+        if (Float.isNaN(a + b)) return false;
+        else if (Float.isInfinite(a) && Float.isInfinite(b)) return true;
+        if (precision != 0) return Math.abs(a - b) <= Math.pow(10, -precision);
+        else return a - b == 0;
 
     }
 
