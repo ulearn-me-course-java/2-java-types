2e879ac91cccf2f054fbd568af9b4e6ce4614ba2
Саша <gorgonova@bk.ru>
2019-02-25 21:52:46 +0500

    Musina task03

--- task03/src/com/example/task03/Task03.java
+++ task03/src/com/example/task03/Task03.java
@@ -4,9 +4,9 @@ public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int metre = centimetre / 100;
 
-        return 0;
+        return metre;
     }
 
     public static void main(String[] args) {
