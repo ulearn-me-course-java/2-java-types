31dd176b63e1b1a76fe3dda082e1849719ebf5a0
Саша <gorgonova@bk.ru>
2019-02-25 22:41:57 +0500

    Musina task12

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -6,18 +6,14 @@ public class Task12 {
 
     public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
 
-        // TODO раскомментируйте и исправьте код
 
         // Считаем проценты за год
 
-        /*
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+            sum = sum.add(sum.multiply(percent)).setScale(9, BigDecimal.ROUND_HALF_UP);
         }
-        return sum;
-        */
 
-        return BigDecimal.ZERO;
+        return sum;
     }
 
     public static void main(String[] args) {
