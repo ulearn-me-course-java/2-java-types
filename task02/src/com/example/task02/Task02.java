31f2833812e5b53a44477eaa55e93acf73a55e1f
Alexey <alexey.pashnin@mail.ru>
2019-06-18 01:57:28 +0400

    Пашнин Алексей. Задание 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,10 +3,14 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        Long number = Long.parseLong(input);
+        String type = "long";
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if (number >= Byte.MIN_VALUE && number <= Byte.MAX_VALUE) type = "byte";
+        else if (number >= Short.MIN_VALUE && number <= Short.MAX_VALUE) type = "short";
+        else if (number >= Integer.MIN_VALUE && number <= Integer.MAX_VALUE) type = "int";
 
-        return "";
+        return type;
     }
 
     public static void main(String[] args) {
