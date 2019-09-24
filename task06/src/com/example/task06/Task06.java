69f5954e91b76b9a28d24c3065b8b089e9da9a99
Валерия Самсонова <samsonova147@hotmail.com>
2019-09-24 23:05:28 +0500

    [Самсонова] Решение задачи 2.6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,26 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int sum = x + y;
+        int count;
+        if (sum == 0)
+            count = 1;
+        else count = 0;
+        while (sum != 0) {
+            count++;
+            sum /= 10;
+        }
 
-        return 0;
+        return count;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+        
     }
 
 }
