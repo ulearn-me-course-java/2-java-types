2a89828898e811455521494ceba84797c06de093
6yryndy4una <semeninoff@gmail.com>
2019-09-24 08:51:11 +0500

    [Семенинов] Задание 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,23 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long fromString = Long.parseLong(input);
+        if ((fromString >= -128) && (fromString <= 127)) {
+            return "byte";
+        } else if ((fromString >= -32768) && (fromString <= 32767)) {
+            return "short";
+        } else if ((fromString >= -2147483648) && (fromString <= 2147483647)) {
+            return "int";
+        } else {
+            return "long";
+        }
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
+        // например вот так
+        //String result = solution("12345");
+        //System.out.println(result);
     }
 
 }
