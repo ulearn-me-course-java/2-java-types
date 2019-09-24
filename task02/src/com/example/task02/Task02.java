f1c5fc7d97fad63718a6acdc695706f40bc48467
V Kalschikov <vkalschikov@gmail.com>
2019-09-24 09:21:27 +0500

    Pass task 2.2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,18 +4,19 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long bigNumber = Long.parseLong(input);
+        if (bigNumber>Integer.MAX_VALUE || bigNumber<Integer.MIN_VALUE) return "long";
+        else if(bigNumber>Short.MAX_VALUE || bigNumber<Short.MIN_VALUE) return "int";
+        else if (bigNumber>Byte.MAX_VALUE || bigNumber<Byte.MIN_VALUE) return "short";
+        else return "byte";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution("12345");
+
+        String result = solution("5");
         System.out.println(result);
-         */
     }
 
 }
