c20e1ac723f5e269ee117d63be7fdfed248a5fa2
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-09-25 00:57:32 +0500

    [Матвеева] Решение задачи 2.2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -6,16 +6,24 @@ public class Task02 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "";
+        long n = Long.parseLong(input);
+        if (n < Byte.MIN_VALUE || n > Byte.MAX_VALUE) {
+            if (n < Short.MIN_VALUE || n > Short.MAX_VALUE) {
+                if (n < Integer.MIN_VALUE || n > Integer.MAX_VALUE)
+                    return "long";
+                return "int";
+            }
+            return "short";
+        }
+        return "byte";
+
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution("12345");
+        String result = solution("-9223372036854775808");
         System.out.println(result);
-         */
     }
 
 }
