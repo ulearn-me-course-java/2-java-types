060dd86db2c2022ddd9f42d2279ea8f614d977ef
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-09-25 11:30:21 +0500

    [Матвеева] Решение задачи 2.7

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -5,17 +5,24 @@ public class Task07 {
     public static int solution(int n, int m, int k) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int row = 0, result = 0, N = n, M = m;
+        while (k <= N) {
+            row++;
+            N -= k;
+        }
+        while (k <= M) {
+            result += row;
+            M -= k;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(10, 20, 5);
+        int result = solution(2, 10, 3);
         System.out.println(result);
-        */
+
     }
 
 }
