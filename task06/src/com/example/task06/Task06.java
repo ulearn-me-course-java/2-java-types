1c8a806f3879ea9fb2f835a4ef1eda23a0852fc3
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-09-25 11:12:25 +0500

    [Матвеева] Решение задачи 2.6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -5,17 +5,15 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        String sum = Integer.toString (x+y);
+        return sum.length();
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+        int result = solution(101, -2);
         System.out.println(result);
-        */
     }
 
 }
