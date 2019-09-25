935781c86da4b9a6e1de04731aa6401996e05866
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-09-25 10:55:07 +0500

    [Матвеева] Решение задачи 2.5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -5,17 +5,22 @@ public class Task05 {
     public static String solution(int x) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        int number = x, digit = 0;
+        while (number > 0) {
+            digit = number % 10;
+            if (digit % 2 != 0)
+                return "FALSE";
+            number /= 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution(1234);
+        String result = solution(123);
         System.out.println(result);
-        */
+
     }
 
 }
