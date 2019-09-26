de90ac9990e64ad4e257857d41ad85bf9dd0c700
Валерия Самсонова <samsonova147@hotmail.com>
2019-09-26 16:19:12 +0500

    [Самсонова] Решение задачи 2.14

--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -5,18 +5,18 @@ public class Task14 {
 
     public static int reverse(int value) {
 
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
+        String str = new StringBuilder(String.valueOf(value)).reverse().toString();
 
-        return 0;
+        return Integer.parseInt(str);
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = reverse(345);
         System.out.println(result);
-         */
+        
     }
 
 
