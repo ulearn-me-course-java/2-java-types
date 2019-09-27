e8b48c947173afc11ebc9b22ef1380156f1eff26
MintAnnetta <mintannetta@gmail.com>
2019-09-27 15:48:38 +0500

    Задача решена

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,15 +3,14 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+	if (x < 10 && ((x % 2) == 0))
+	    return "TRUE";
+	if (x > 10 && ((x % 2) == 0))
+	    return solution(x / 10);
         return "FALSE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
         /*
         String result = solution(1234);
         System.out.println(result);
