cf4d73430d0733e7740081a0c285bc917b303534
MintAnnetta <mintannetta@gmail.com>
2019-09-27 16:10:22 +0500

    Задача решена

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,15 +3,13 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        String sumstr = Integer.toString(x + y);
+	if (sumstr.startsWith("-"))
+	    return sumstr.length() - 1;
+        return sumstr.length();
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
         /*
         int result = solution(12, 34);
         System.out.println(result);
