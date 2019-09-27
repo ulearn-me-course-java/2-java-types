f9bce8000761aac4271ee9d35b6d3a10c5371e07
MintAnnetta <mintannetta@gmail.com>
2019-09-27 14:17:10 +0500

    Закомментировано ручное тестирование

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -14,8 +14,8 @@ public class Task02 {
     }
 
     public static void main(String[] args) {
-	String result = solution("-35000");
-        System.out.println(result);
+	// String result = solution("-35000");
+        // System.out.println(result);
     }
 
 }
