44eb99b1c017b984df5546f3bea7aa7a22e54a90
nesquikk <petniza@yandex.ru>
2019-09-27 18:57:33 +0500

    [Хазов] Задание 2.5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -4,9 +4,13 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        while (x != 0) {
+            if (x % 2 != 0)
+                return "FALSE";
+            else
+                x = x / 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
