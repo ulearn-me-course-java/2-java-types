c00d5d02192c68214ced2473ea8195bb5e001e6d
MintAnnetta <mintannetta@gmail.com>
2019-09-27 14:16:14 +0500

    Задача решена

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,19 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+	Long num = Long.parseLong(input);
+	if (num >= Byte.MIN_VALUE && num <= Byte.MAX_VALUE)
+	    return "byte";
+	if (num >= Short.MIN_VALUE && num <= Short.MAX_VALUE)
+	    return "short";
+	if (num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE)
+	    return "int";
+	return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
+	String result = solution("-35000");
         System.out.println(result);
-         */
     }
 
 }
