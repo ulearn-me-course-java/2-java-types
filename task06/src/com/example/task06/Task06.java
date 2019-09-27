d4919bf47d6e6034ba07f97157b91a41db595c3c
nesquikk <petniza@yandex.ru>
2019-09-27 19:03:26 +0500

    [Хазов] Задание 2.6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,16 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        String res = Integer.toString(x + y);
+        if (x + y >= 0)
+            return res.length();
+        else
+            return res.length() - 1;
         return 0;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
-        System.out.println(result);
-        */
+
     }
 
 }
