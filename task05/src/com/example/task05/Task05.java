68e8bae2ec847e3bb18f96dcfbc4548a08a6c2e6
rokoman13 <rokoman13@users.noreply.github.com>
2019-12-09 15:28:42 +0500

    Popov Evgeny task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -4,9 +4,15 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        int number = x;
+        while (number !=0) {
+            if ((number % 10) % 2 !=0)
+            {
+                return "FALSE";
+            }
+            number /= 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
