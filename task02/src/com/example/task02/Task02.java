0bf0f4d6540c7c72ea0c7c61a68666e41f06f591
NikMarcos <eronikita@yandex.ru>
2019-12-18 10:56:01 +0500

    task02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -6,16 +6,25 @@ public class Task02 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "";
+        long x = Long.valueOf(input);
+        if (x < Byte.MAX_VALUE && x > Byte.MIN_VALUE){
+            return "byte";
+        } else if (x < Short.MAX_VALUE && x > Short.MIN_VALUE) {
+            return "short";
+        } else if (x < Integer.MAX_VALUE && x > Integer.MIN_VALUE) {
+            return "int";
+        } else {
+            return "long";
+        }
+
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
