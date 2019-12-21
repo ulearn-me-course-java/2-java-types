66ef1b5f2665a20289341c298e7fc26481d76d2b
NikMarcos <eronikita@yandex.ru>
2019-12-21 16:40:13 +0500

    task06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,20 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = x + y;
+        String z = Integer.toString(sum);
+        char[] arr = z.toCharArray();
+        int len = arr.length;
+        return len;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+
+        int result = solution(12, 343);
         System.out.println(result);
-        */
+
     }
 
 }
