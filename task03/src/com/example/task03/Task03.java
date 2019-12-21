e663bda4b8084611ada30018b4fc991ef1793ae7
Alexandr Dolgov <aydolgov@gmail.com>
2019-12-21 23:08:00 +0500

    task03

--- task03/src/com/example/task03/Task03.java
+++ task03/src/com/example/task03/Task03.java
@@ -1,21 +1,24 @@
 package com.example.task03;
 
+import static java.lang.Math.*;
+
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        double del = (double) centimetre / 100;
+        int metres = (int) floor(del);
+        return metres;
 
-        return 0;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = getMetreFromCentimetre(345);
+
+        double result = getMetreFromCentimetre(270);
         System.out.println(result);
-         */
+
     }
 
 }
