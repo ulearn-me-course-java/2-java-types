3ce8f23b2d0e2bc95ffe5a01a23a3a6065a4653d
NikMarcos <eronikita@yandex.ru>
2019-12-21 17:09:07 +0500

    task07

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -4,18 +4,20 @@ public class Task07 {
 
     public static int solution(int n, int m, int k) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int campSquare = n * m;
+        int tentSquare = (int) Math.pow(k, 2);
+        double tentNum = campSquare / tentSquare;
+        int res = (int) Math.floor(tentNum);
+        return res;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(10, 20, 5);
+
+        int result = solution(10, 10, 2);
         System.out.println(result);
-        */
+
     }
 
 }
