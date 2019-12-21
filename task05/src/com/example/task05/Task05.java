7c16921d5f175da099f3650f642e0acc30a79e08
NikMarcos <eronikita@yandex.ru>
2019-12-21 15:48:28 +0500

    task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -1,21 +1,30 @@
 package com.example.task05;
 
+import java.util.Arrays;
+
 public class Task05 {
 
-    public static String solution(int x) {
+    public static String solution(int number) {
+        String str = Integer.toString(number);
+        char[] arr = str.toCharArray();
+        for(int x : arr ) {
+            if (x % 2 != 0) {
+                return "FALSE";
+            } else {
+                continue;
+            }
+        }
+        return "TRUE";
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution(1234);
+
+        String result = solution(2254);
         System.out.println(result);
-        */
+
     }
 
 }
