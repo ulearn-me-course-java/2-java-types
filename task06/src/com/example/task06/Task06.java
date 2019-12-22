22c0fe1842567ca542b9bdeb53afdb7f12359a30
NikMarcos <eronikita@yandex.ru>
2019-12-22 11:26:39 +0500

    task06 - Math.abs added

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -5,7 +5,7 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         int sum = x + y;
-        String z = Integer.toString(sum);
+        String z = Integer.toString(Math.abs(sum));
         char[] arr = z.toCharArray();
         int len = arr.length;
         return len;
@@ -15,7 +15,7 @@ public class Task06 {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        int result = solution(12, 343);
+        int result = solution(-199, -343);
         System.out.println(result);
 
     }
