0e79cfb32651acc602558d45b27ac53ba307d350
NikMarcos <eronikita@yandex.ru>
2019-12-23 22:12:28 +0500

    task13

--- task13/src/com/example/task13/Task13.java
+++ task13/src/com/example/task13/Task13.java
@@ -4,18 +4,18 @@ public class Task13 {
 
     public static char toUpperCase(char c) {
 
-        // TODO привести букву к верхнему регистру
+        char ch3 = Character.toUpperCase(c);
 
-        return c;
+        return ch3;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        char result = solution('x');
+
+        char result = toUpperCase('a');
         System.out.println(result);
-         */
+
     }
 
 }
