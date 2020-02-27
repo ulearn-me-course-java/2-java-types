07cd2102e6a917dd9eb4f74862a578db2b3f683a
ViktoryaInn <viki.2000@list.ru>
2020-02-27 19:03:21 +0500

    Губайдуллина Виктория Задание2.5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,18 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        String num = Integer.toString(x);
+        if(num.contains("1") || num.contains("3") || num.contains("5") || num.contains("7") || num.contains("9"))
+            return "FALSE";
+        else return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution(1234);
+
+        String result = solution(28);
         System.out.println(result);
-        */
     }
 
 }
