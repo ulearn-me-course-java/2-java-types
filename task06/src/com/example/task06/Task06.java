e8e6c89fbc7991a8c81216a701fa4fab9ab852b5
DarenOwl <Charly.Fox@mail.ru>
2020-02-27 20:23:28 +0500

    Андреева Юлия. Задание06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,19 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int z = x + y;
+        int i = 0;
+        do {
+            z = z / 10;
+            i++;
+        } while (z != 0);
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return i;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
+        int result = solution(99, 99);
         System.out.println(result);
-        */
     }
 
 }
