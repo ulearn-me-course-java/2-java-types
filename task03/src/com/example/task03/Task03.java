791e5c75230e5e51cffe9f253f1a36f1279a8678
ViktoryaInn <viki.2000@list.ru>
2020-02-27 18:15:17 +0500

    Губайдуллина Виктория Задание2.3

--- task03/src/com/example/task03/Task03.java
+++ task03/src/com/example/task03/Task03.java
@@ -1,21 +1,17 @@
 package com.example.task03;
+import java.math.*;
 
 public class Task03 {
 
     public static int getMetreFromCentimetre(int centimetre) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        double metre = centimetre * 0.01;
+        return (int)metre;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = getMetreFromCentimetre(345);
+
+        int result = getMetreFromCentimetre(465);
         System.out.println(result);
-         */
     }
 
 }
