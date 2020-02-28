db4fafbb8233d5fbddac5d0f870c158264037dc6
Ekaterina <malyavkina-00@mail.ru>
2020-02-28 10:58:49 +0500

    Малявкина Екатерина Задание5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,7 +3,7 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-        while (x>-1)
+        while (x>0)
             if ((x%10)%2 != 0)
                 return "FALSE";
             x/=10;
