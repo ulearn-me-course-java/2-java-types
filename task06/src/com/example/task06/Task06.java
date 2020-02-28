5572cd43e2271f54d849b7fef07442383abd6d5e
Ekaterina <malyavkina-00@mail.ru>
2020-02-28 10:47:03 +0500

    Малявкина Екатерина Задание6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,22 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int num = x + y;
+        int kol = 0;
+        while (num>-1){
+            x/=10;
+            kol++;
+        }
+        return kol;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
