0ad36e526bfaf78eb2455a26393086b90d1eb526
Unknown <unknown@example.com>
2020-02-29 14:58:07 +0500

    Второе задание

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,23 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
+        long value = Long.parseLong(input);
+        if ((Byte.MIN_VALUE <= value) & (value <= Byte.MAX_VALUE))
+            return "byte";
+        else if ((Short.MIN_VALUE <= value) & (value <= Short.MAX_VALUE))
+            return "short";
+        else if ((Integer.MIN_VALUE <= value) & (value <= Integer.MAX_VALUE))
+            return "int";
+        else if ((Long.MIN_VALUE <= value) & (value <= Long.MAX_VALUE))
+            return "long";
         return "";
     }
 
+
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
