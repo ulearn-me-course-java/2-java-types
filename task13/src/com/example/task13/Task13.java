627704a46cc49f32ce8e8a3354c344da0215b15d
DarenOwl <Charly.Fox@mail.ru>
2020-02-29 16:45:39 +0500

    Андреева Юлия. Задание13

--- task13/src/com/example/task13/Task13.java
+++ task13/src/com/example/task13/Task13.java
@@ -3,10 +3,7 @@ package com.example.task13;
 public class Task13 {
 
     public static char toUpperCase(char c) {
-
-        // TODO привести букву к верхнему регистру
-
-        return c;
+        return (char)(c - 0x20);
     }
 
     public static void main(String[] args) {
