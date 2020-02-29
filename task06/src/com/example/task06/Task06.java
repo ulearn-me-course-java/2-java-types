ae0cfdcb883e82ffad1e75e8f735f72cdee13a6f
Unknown <unknown@example.com>
2020-02-29 19:00:20 +0500

    Шестое задание

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,42 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        x = x + y;
+        int i = 1;
+        if(x == 0)
+        {
+            return i;
+        }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        if(x > 0) {
+            while (x >= 0) {
+                if (x == 0){
+                    return i - 1;
+                }
+                int z = x % 10; //Получаем последнее число
+                i++;
+                x = x / 10; //Иначе затираем и идём дальше
+            }
+        }
+        else {
+            while (x <= 0) {
+                if (x == 0) {
+                    return i - 1;
+                }
+                int z = x % 10; //Получаем последнее число
+                i++;
+                x = x / 10; //Иначе затираем и идём дальше
+            }
+        }
+        return i;
     }
 
+
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
+
+        int result = solution(1, 1);
         System.out.println(result);
-        */
+
     }
 
 }
