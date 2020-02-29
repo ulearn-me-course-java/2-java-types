b675339ad861b0eff12a95db7a3da7cb0f2ed8cd
Dauren <baidau.rk.a@gmail.com>
2020-02-29 16:14:21 +0500

    Revert "Байманов Даурен.задание 07"
    This reverts commit d45cac77abd9f59f715150f3fbd0898779f5318e.

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -3,7 +3,10 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-        return n / k * (m / k);
+
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+
+        return 0;
     }
 
     public static void main(String[] args) {
