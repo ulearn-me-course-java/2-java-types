223a1064ab0d9e3a7ce8b554c3a8993d156cf519
Dauren <baidau.rk.a@gmail.com>
2020-02-29 14:32:48 +0500

    Байманов Даурен.задание 04

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -1,21 +1,34 @@
 package com.example.task04;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task04 {
+    @FunctionalInterface
+    interface Operationable {
+        float calculate(int x, int y);
+    }
+    static Map<String, Operationable> operations = getDefaultOperation();
+
+    private static Map<String, Operationable> getDefaultOperation() {
+        Map<String, Operationable> operation = new HashMap<>();
+        operation.put("+", (x, y) -> (float) x + y);
+        operation.put("-", (x, y) -> (float) x - y);
+        operation.put("*", (x, y) -> (float) x * y);
+        operation.put("/", (x, y) -> (float) x / y);
+        return operation;
+    }
 
     public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return operations.get(operation)
+                .calculate(a, b);
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
     }
 
 }
