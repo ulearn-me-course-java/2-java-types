b9dbfb5cc54179d1fd0b6d81060149b640448801
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-01 23:47:19 +0500

    Гадимов Наби. Задание 3

--- task03/src/com/example/task03/Task03.java
+++ task03/src/com/example/task03/Task03.java
@@ -1,21 +1,17 @@
 package com.example.task03;
 
 public class Task03 {
-
-    public static int getMetreFromCentimetre(int centimetre) {
-
+    public static int getMetreFromCentimetre(int cm) {
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return cm/100;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = getMetreFromCentimetre(345);
-        System.out.println(result);
-         */
+        int cm = getMetreFromCentimetre(345);
+        System.out.println(cm);
+
     }
 
 }
