8af160814d884001a3cee5d9bab0465e268d6ebc
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-01 14:24:46 +0500

    Гадимов Наби. Задание 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -1,21 +1,22 @@
 package com.example.task02;
 
 public class Task02 {
-
     public static String solution(String input) {
+        long a = Long.parseLong(input);
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        if (a >= Byte.MIN_VALUE && a <= Byte.MAX_VALUE) {
+            return "byte";
+        }
+        else if (a >= Short.MIN_VALUE && a <= Short.MAX_VALUE) {
+            return "short";
+        }
+        else if (a >= Integer.MIN_VALUE && a <= Integer.MAX_VALUE) {
+            return "int";
+        }
+        return "long";
     }
-
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
+        String result = solution("50000000");
         System.out.println(result);
-         */
     }
-
 }
