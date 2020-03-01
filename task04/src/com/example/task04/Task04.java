67f3839e5a95680fab55f421101afff33b2cf741
hackerzhenya <zhenya92.ru@gmail.com>
2020-03-02 03:38:54 +0500

    Неверояяяяяяяяяяяятно весело

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -1,12 +1,22 @@
 package com.example.task04;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
+        return makeCalculator().get(operation).apply((float) a, (float) b);
+    }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+    private static Map<String, BiFunction<Float, Float, Float>> makeCalculator() {
+        return new HashMap<String, BiFunction<Float, Float, Float>>() {{
+            put("+", Float::sum);
+            put("-", (x, y) -> x - y);
+            put("*", (x, y) -> x * y);
+            put("/", (x, y) -> x / y);
+        }};
     }
 
     public static void main(String[] args) {
