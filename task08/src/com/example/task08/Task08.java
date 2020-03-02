eb41038cc610ba471e59db401e4d0286318070ee
Ekaterina <malyavkina-00@mail.ru>
2020-03-02 08:21:30 +0500

    Малявкина Екатерина Задание №8

--- task08/src/com/example/task08/Task08.java
+++ task08/src/com/example/task08/Task08.java
@@ -3,9 +3,9 @@ package com.example.task08;
 public class Task08 {
 
     public static boolean solution() {
-        // TODO измените тип и значение переменной x, чтобы возвращалось значение true
+        double x = Double.NEGATIVE_INFINITY;
+        //double x = Double.POSITIVE_INFINITY;
 
-        int x = 100;
         return x == x + 1;
     }
 
