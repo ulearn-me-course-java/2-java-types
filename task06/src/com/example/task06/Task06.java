f6fb355e0e45dfed4630e71afadcf724159840af
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-03 00:58:58 +0500

    Гадимов Наби. Задание 6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,25 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int z = x + y;
+        if (z == 0){
+            return 1;
+        }
+        int i = 0;
+        while (z != 0){
+            z = z/10;
+            i++;
+        }
+        return i;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+
+        int result = solution(1, 9999);
         System.out.println(result);
-        */
+
     }
 
 }
