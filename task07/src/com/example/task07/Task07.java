5d4d6c47d0a9f4c56d26d03ac60b1d1052e748c6
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-03 01:05:22 +0500

    Гадимов Наби. Задание 7

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -4,18 +4,20 @@ public class Task07 {
 
     public static int solution(int n, int m, int k) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int n1 = n / k;
+        int m1 = m / k;
+        int result = n1 * m1;
 
-        return 0;
+        return result;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(10, 20, 5);
+
+        int result = solution(20, 50, 3);
         System.out.println(result);
-        */
+
     }
 
 }
