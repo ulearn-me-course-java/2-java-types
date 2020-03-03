0ee3606ed318171558123800a8c0064c75bf043d
UnuntriumIIT <letsplay236@gmail.com>
2020-03-03 21:52:22 +0500

    Task 2.6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -1,21 +1,16 @@
 package com.example.task06;
 
 public class Task06 {
-
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = x + y;
+        int counter = 0;
+        do{
+            counter++;
+            sum = sum / 10;
+        }while (sum != 0);
+        return counter;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
-        System.out.println(result);
-        */
     }
-
 }
