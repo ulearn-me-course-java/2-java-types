08f960b1bffea7b6b23202748ecacd5864e5294a
MatveyTolpeev <61470886+MatveyTolpeev@users.noreply.github.com>
2020-03-03 23:45:16 +0500

    Update Task05.java

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -6,7 +6,10 @@ public class Task05 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
+        String a = Integer.toString(x);
+       if(a.indexOf('1') != -1 || a.indexOf('3') != -1 || a.indexOf('5') != -1 || a.indexOf('7') != -1  || a.indexOf('9') != -1 )
         return "FALSE";
+       else return "TRUE";
     }
 
     public static void main(String[] args) {
