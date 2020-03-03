b9066bc6910d690430079ba045cc42c5f42fbc6d
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-04 00:45:24 +0500

    Гадимов Наби. Задание 14

--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -6,17 +6,17 @@ public class Task14 {
     public static int reverse(int value) {
 
         // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
-
-        return 0;
+        String num = new StringBuilder(Integer.toString(value)).reverse().toString(); //преобразование числа в строку
+        return Integer.parseInt(num); //преобразование строки в число
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = reverse(345);
         System.out.println(result);
-         */
+
     }
 
 
