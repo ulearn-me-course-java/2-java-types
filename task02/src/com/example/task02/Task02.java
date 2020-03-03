7b11ce6dced612f8e4ef31bd1c7810301d0811d5
UnuntriumIIT <letsplay236@gmail.com>
2020-03-03 20:53:10 +0500

    Task 2.2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -1,21 +1,28 @@
 package com.example.task02;
 
+import java.math.BigInteger;
+
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        BigInteger in = new BigInteger(input);
+        if (in.compareTo(new BigInteger("-129"))
+                > in.compareTo(new BigInteger("128")))
+            return "byte";
+        else if (in.compareTo(new BigInteger("-32769"))
+                > in.compareTo(new BigInteger("32768")))
+            return "short";
+        else if (in.compareTo(new BigInteger("-2147483649"))
+                > in.compareTo(new BigInteger("2147483648")))
+            return "int";
+        else if (in.compareTo(new BigInteger("-9223372036854775809"))
+                > in.compareTo(new BigInteger("9223372036854775808")))
+            return "long";
+        else
+            return "very big number";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
-    }
 
+    }
 }
