d6e6490fe23b3dfdebd28d3ad27dd5f0f76d8909
Stasenash <staslisenash@gmail.com>
2020-03-04 21:22:50 +0500

    Сидорчук Анастасия. З.04

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -3,10 +3,10 @@ package com.example.task04;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
-        if (operation=="+") return a+b;
-        else if (operation=="-") return a-b;
-        else if (operation=="*") return a*b;
-        else return a/b;
+
+        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+
+        return 0;
     }
 
     public static void main(String[] args) {
