bfc4604b7cf8ae097bf521bc7a60ebb36f2fc33f
Stasenash <staslisenash@gmail.com>
2020-03-04 21:37:47 +0500

    Сидорчук Анастасия. З.06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,9 +4,16 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int sum=x+y;
+        int count=0;
 
-        return 0;
+        if(sum==0) return 1;
+
+        while(sum!=0){
+            sum/=10;
+            count++;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
