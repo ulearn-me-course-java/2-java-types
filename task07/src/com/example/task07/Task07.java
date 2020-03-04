ab1cd9f08d220737b39a962af9776cc1021e4239
Stasenash <staslisenash@gmail.com>
2020-03-04 22:33:08 +0500

    Сидорчук Анастасия. З.07

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -3,7 +3,7 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-        return (n-n%k)*(m-m%k)/(k*k);
+        return (n/k)*(m/k);
     }
 
     public static void main(String[] args) {
