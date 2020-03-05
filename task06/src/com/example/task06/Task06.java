f73efd831d6f31de43078f93cca3c8fd03023653
Dauren <baidau.rk.a@gmail.com>
2020-03-05 21:13:49 +0500

    Жмакина Алина. Задание 2.6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -2,20 +2,27 @@ package com.example.task06;
 
 public class Task06 {
 
-    public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+  public static int solution(int x, int y) {
+    int result = 0;
+    int z = x + y;
+    if (z == 0) {
+      result = 1;
+      return result;
     }
+    while (z != 0) {
+      result++;
+      z /= 10;
+    }
+    return result;
+  }
 
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+  public static void main(String[] args) {
+    // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+    // например вот так:
         /*
         int result = solution(12, 34);
         System.out.println(result);
         */
-    }
+  }
 
 }
