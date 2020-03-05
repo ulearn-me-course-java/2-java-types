c262efa4f87cd49fd01c127117e83d7b34056738
hackerzhenya <zhenya92.ru@gmail.com>
2020-03-06 00:19:10 +0500

    task 14

--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -1,23 +1,40 @@
 package com.example.task14;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task14 {
 
-
     public static int reverse(int value) {
+        String reversed = String
+                .valueOf(value)
+                .chars()
+                .mapToObj(Task14::castToChar)
+                .collect(reverseStream())
+                .reduce(new StringBuilder(), StringBuilder::append, StringBuilder::append)
+                .toString();
 
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
+        return Integer.parseInt(reversed);
+    }
 
-        return 0;
+    static <T> Collector<T, ?, Stream<T>> reverseStream() {
+        return Collectors.collectingAndThen(Collectors.toList(), Task14::reverseCollection);
+    }
+
+    static <T> Stream<T> reverseCollection(List<T> list) {
+        Collections.reverse(list);
+        return list.stream();
+    }
+
+    static char castToChar(int i) {
+        return (char) i;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = reverse(345);
         System.out.println(result);
-         */
     }
-
-
 }
