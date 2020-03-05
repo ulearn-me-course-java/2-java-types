4100ab9fb9634efbf180e01c1e5539eb6a25cc6a
Dauren <baidau.rk.a@gmail.com>
2020-03-05 21:40:50 +0500

    Жмакина Алина. Задание 2.12

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -4,31 +4,20 @@ import java.math.BigDecimal;
 
 public class Task12 {
 
-    public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
+  public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
+    BigDecimal x = percent.add(new BigDecimal(1.0));
+    return sum.multiply(x.pow(12)).setScale(9, BigDecimal.ROUND_HALF_UP);
+  }
 
-        // TODO раскомментируйте и исправьте код
+  public static void main(String[] args) {
 
-        // Считаем проценты за год
+    BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
+    BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
 
-        /*
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
-        */
+    sum = benefit(sum, percent);
 
-        return BigDecimal.ZERO;
-    }
+    System.out.println("Сумма на счете через год: " + sum);
 
-    public static void main(String[] args) {
-
-        BigDecimal sum = new BigDecimal(500).setScale(9, BigDecimal.ROUND_HALF_UP); // 500 руб. на счете
-        BigDecimal percent = new BigDecimal(0.00000001f).setScale(9, BigDecimal.ROUND_HALF_UP); // 0.000001% ежемесячно
-
-        sum = benefit(sum, percent);
-
-        System.out.println("Сумма на счете через год: " + sum);
-
-    }
+  }
 
 }
