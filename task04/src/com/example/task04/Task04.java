d5dbb15851d314e5e566fb36cbf530f1736b928e
Dauren <baidau.rk.a@gmail.com>
2020-03-05 21:08:11 +0500

    Жмакина Алина. Задание 2.4

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -2,20 +2,34 @@ package com.example.task04;
 
 public class Task04 {
 
-    public static float calculate(int a, int b, String operation) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+  public static float calculate(int a, int b, String operation) {
+    float result;
+    switch (operation) {
+      case ("+"):
+        result = a + b;
+        break;
+      case ("-"):
+        result = a - b;
+        break;
+      case ("/"):
+        result = (float) a / (float) b;
+        break;
+      case ("*"):
+        result = a * b;
+        break;
+      default:
+        throw new IllegalStateException("Unexpected value: " + operation);
     }
+    return result;
+  }
 
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
+  public static void main(String[] args) {
+    // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+    // например вот так:
         /*
         float result = calculate(-25, 5, "/");
         System.out.println(result);
         */
-    }
+  }
 
 }
