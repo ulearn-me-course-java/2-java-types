cac11a258de5b488bfd588c02280f0cab84b0970
hackerzhenya <zhenya92.ru@gmail.com>
2020-03-06 00:05:02 +0500

    beautify

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,30 +3,32 @@ package com.example.task02;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+class Int {
+    public static final int MIN_VALUE = Integer.MIN_VALUE;
+    public static final int MAX_VALUE = Integer.MAX_VALUE;
+}
+
 public class Task02 {
 
     public static String solution(String input) {
         // ================== Я не ищу легких путей! ==================
 
-        String type = Stream
-                .of(Byte.class, Short.class, Integer.class, Long.class)
+        return Stream
+                .of(Byte.class, Short.class, Int.class, Long.class)
                 .filter(makeComparator(input))
                 .findFirst()
                 .get()
                 .getSimpleName()
                 .toLowerCase();
-
-        return type.equals("integer") ? "int" : type;
     }
 
-
-    private static Predicate<Class<? extends Number>> makeComparator(String input) {
+    private static Predicate<Class<?>> makeComparator(String input) throws RuntimeException {
         long number = Long.parseLong(input);
 
         return primitive -> {
             try {
                 return number >= primitive.getField("MIN_VALUE").getLong(null) &&
-                        number <= primitive.getField("MAX_VALUE").getLong(null);
+                       number <= primitive.getField("MAX_VALUE").getLong(null);
             } catch (Exception e) {
                 return false;
             }
