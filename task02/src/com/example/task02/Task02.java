4c0f9a86323ff623d919e4474349ba4026dc627f
Grin <grinlikegreenbutgrin@gmail.com>
2020-03-05 22:00:30 +0500

    task 2.2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,35 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        try {
+            Long.parseLong(input);
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        try {
+            Integer.parseInt(input);
+        } catch (Exception e) {
+            return "long";
+        }
 
-        return "";
+        try {
+            Short.parseShort(input);
+        } catch (Exception e) {
+            return "int";
+        }
+
+        try {
+            Byte.parseByte(input);
+        } catch (Exception e) {
+            return "short";
+        }
+        return "byte";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
