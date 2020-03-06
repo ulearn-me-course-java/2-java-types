a47420e4068f78777688001f86bd44fb01a0f723
Jin <jin1310@icloud.com>
2020-03-06 11:13:58 +0500

    Ширшов Илья 04

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,24 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if(x+y==0) return 1;
+        int result=0;
+        int sum=x+y;
+        while(sum!=0) {
+            sum /= 10;
+            result++;
+        }
+
+
 
         return 0;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
