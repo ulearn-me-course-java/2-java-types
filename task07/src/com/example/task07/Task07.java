81d251d6e4c8ecc196cce02e0a159e226d95385e
Jin <jin1310@icloud.com>
2020-03-06 11:16:03 +0500

    Ширшов Илья 07

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -4,9 +4,9 @@ public class Task07 {
 
     public static int solution(int n, int m, int k) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int areaS = n * m;
+        int tentS = k * k;
+        return areaS/tentS;
     }
 
     public static void main(String[] args) {
