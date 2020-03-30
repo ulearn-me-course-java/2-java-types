027eafbf6cf4512144e160893bf488b1d890a1b7
Ekaterina <slalam.ww@gmail.com>
2020-03-30 16:26:16 +0500

    Абрамкова, 2.02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -1,21 +1,24 @@
 package com.example.task02;
+import java.util.Scanner;
 
 public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        long num = Long.parseLong(input);
+        System.out.println("type of your number:");
 
-        return "";
+        if(num>=-128 && num<=127) return "byte";
+        else if (num>=-32_768 && num<=32_767) return "short";
+        else if (num>=-2_147_483_648 && num<=2_147_483_647) return "int";
+        else return "long";
+
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
+        String result = solution("567");
         System.out.println(result);
-         */
+
     }
 
 }
