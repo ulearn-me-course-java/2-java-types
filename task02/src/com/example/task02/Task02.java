0ebb2f04ce46104856f5e199957272ce99cd3ac4
redrockstyle <ulowpie@gmail.com>
2020-09-13 23:17:40 +0500

    [Khazov Anton]

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -5,17 +5,23 @@ public class Task02 {
     public static String solution(String input) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long val = Long.valueOf(input);
+        if (val <= Byte.MAX_VALUE && val >= Byte.MIN_VALUE){
+            return "byte";
+        } else if (val <= Short.MAX_VALUE && val >= Short.MIN_VALUE){
+            return "short";
+        } else if (val <= Integer.MAX_VALUE && val >= Integer.MIN_VALUE){
+            return "int";
+        } else return "long";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
         /*
-        String result = solution("12345");
+        String result = solution("123");
         System.out.println(result);
-         */
+        */
     }
 
 }
