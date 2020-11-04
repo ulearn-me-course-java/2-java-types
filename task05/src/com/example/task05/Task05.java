544a42ff13baba6cb185e196476abede73419fb5
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-11-04 19:14:53 +0500

    Ледянкин Павел Задание 2.05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -1,21 +1,24 @@
 package com.example.task05;
 
-public class Task05 {
-
-    public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+public class Task05
+{
+    public static String solution(int x)
+    {
+        int digit;
+        while (x > 0)
+        {
+            digit = x % 10;
+            if (digit % 2 == 1)
+                return "FALSE";
+            x = x / 10;
+        }
+        return "TRUE";
     }
 
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
+    public static void main(String[] args)
+    {
+        String result = solution(8204);
         System.out.println(result);
-        */
     }
-
 }
+
