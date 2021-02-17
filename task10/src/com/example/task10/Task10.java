4eeecb7950e933e8184f80bacaf41b5dac24393a
M <mihail.petuxov@yandex.ru>
2021-02-17 09:56:16 +0500

    Петухов Михаил. Задача 2.10

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -4,18 +4,8 @@ public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
 
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-
-        if (Float.isInfinite(a) && Float.isInfinite(b))
-            return true;
-        if (Float.isInfinite(a) || Float.isInfinite(b))
-            return false;
-        if (Float.isNaN(a) && Float.isNaN(b))
-            return true;
-        return Math.abs(a - b) < precision;
-
+        float abs = Math.abs(a-b);
+        return a == b || Float.isNaN(a) && Float.isNaN(b) || abs < Math.pow(0.1, precision);
     }
 
     public static void main(String[] args) {
