9599f16b239b2b797cac6300919abdbf1f966b2f
Evgeniy <djonich@mail.ru>
2021-02-21 18:15:58 +0500

    Ничипоренко Евгений. Задание 2.2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,8 +3,6 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
         long number = Long.parseLong(input);
         if(Byte.MIN_VALUE <= number && Byte.MAX_VALUE>=number)
             return "byte";
@@ -17,12 +15,8 @@ public class Task02 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
