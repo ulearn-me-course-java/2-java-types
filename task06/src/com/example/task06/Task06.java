ad463c01a7ca53fc92df008a9c5e5bbf28ab39d2
Evgeniy <djonich@mail.ru>
2021-02-21 19:25:39 +0500

    Ничипоренко Евгений. Задание 2.6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,10 +3,13 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int length = 0;
+        int sum = x + y;
+        do {
+            sum/=10;
+            length++;
+        }while (sum!=0);
+        return length;
     }
 
     public static void main(String[] args) {
