17f0d76c4bd88ce233e5f57c153ebdf005fbb62d
Wither87 <wither_87@mail.ru>
2021-02-25 01:17:00 +0500

    Крутолевич Сергей Задача 2.4

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -3,19 +3,30 @@ package com.example.task04;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
+        float result = 0f;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if (operation.equals("+")){
+            result = (float)a + b;
+        }
+        else if (operation.equals("-")){
+            result = (float)a - b;
+        }
+        else if (operation.equals("*")){
+            result = (float)a * b;
+        }
+        else if (operation.equals("/")){
+            result = (float)a / b;
+        }
 
-        return 0;
+        return result;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
