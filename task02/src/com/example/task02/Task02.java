9d6ac8291554703befed5c86e936bca78c90396e
1 <hstllbg@gmail.com>
2021-02-24 20:21:22 +0500

    Петров Игорь. Задание 2.2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,16 +3,14 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-        double value = Double.parseDouble(input);
+        long value = Long.parseLong(input);
         if (value <= Byte.MAX_VALUE && value >= Byte.MIN_VALUE)
             return "byte";
         if (value <= Short.MAX_VALUE && value >= Short.MIN_VALUE)
             return "short";
         if (value <= Integer.MAX_VALUE && value >= Integer.MIN_VALUE)
             return "int";
-        if (value <= Long.MAX_VALUE && value >= Long.MIN_VALUE)
-            return "long";
-        return "";
+        return "long";
     }
 
     public static void main(String[] args) {
