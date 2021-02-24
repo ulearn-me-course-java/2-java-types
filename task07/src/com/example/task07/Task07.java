357e130ed77cb0fdb470bb55320bb88145f707ba
пк <nikitos.wie@mail.ru>
2021-02-24 22:02:26 +0300

    Яхин Никита - Task_07

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -3,7 +3,7 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-        return (n*m)/(k*k);
+        return (n/k)*(m/k);
     }
 
     public static void main(String[] args) {
