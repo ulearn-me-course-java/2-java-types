d643375f96893a7c403a8533404df3c3624771e8
Wither87 <wither_87@mail.ru>
2021-02-25 01:19:09 +0500

    Крутолевич Сергей Задача 2.5

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,24 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int result = x + y;
+        if (result == 0)
+            return 1;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int length = 0;
+        while (result != 0){
+            result /= 10;
+            length++;
+        }
+        return length;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
