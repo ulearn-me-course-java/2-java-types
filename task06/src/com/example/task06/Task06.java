d1aa543c7a449ed49000b0d035f6ae1f0d1c3a98
eunery <asdad@yaofj.com>
2021-02-24 19:27:57 +0500

    Кулагин Сергей Task06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -5,17 +5,24 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int z = x+y;
+        boolean flag = true;
+        int counter = 0;
+        while (flag){
+            if (z/10 == 0) flag = false;
+            counter++;
+            z /= 10;
+        }
+        return counter;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
