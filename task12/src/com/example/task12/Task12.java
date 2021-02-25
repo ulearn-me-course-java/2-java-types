d40e8f4c40668ca3b495570e4452855a32b97935
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2021-02-25 18:37:55 +0500

    Update Task12.java

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -5,19 +5,12 @@ import java.math.BigDecimal;
 public class Task12 {
 
     public static BigDecimal benefit(BigDecimal sum, BigDecimal percent) {
-
-        // TODO раскомментируйте и исправьте код
-
         // Считаем проценты за год
 
-        /*
         for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
+            sum = sum.multiply(percent.add(BigDecimal.ONE));
         }
-        return sum;
-        */
-
-        return BigDecimal.ZERO;
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
     }
 
     public static void main(String[] args) {
