3c9f61407f1e2ad966306e6d702f10b78d13ec3a
rewqty <boss.dima0000@gmail.com>
2021-02-25 17:01:11 +0500

    Челноков Дмитрий Task13

--- task13/src/com/example/task13/Task13.java
+++ task13/src/com/example/task13/Task13.java
@@ -3,7 +3,6 @@ package com.example.task13;
 public class Task13 {
 
     public static char toUpperCase(char c) {
-
         return (char) (c - 32);
 
         // Либо просто вызвать toUpperCase() метод класса Character
