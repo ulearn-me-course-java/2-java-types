52a43e31b9554b5a83958f6c417c12eeba14d035
DarkLight <max.gubar@outlook.com>
2021-02-25 15:50:55 +0500

    Гуарь Максим task13

--- task13/src/com/example/task13/Task13.java
+++ task13/src/com/example/task13/Task13.java
@@ -7,12 +7,6 @@ public class Task13 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        char result = solution('x');
-        System.out.println(result);
-         */
     }
 
 }
