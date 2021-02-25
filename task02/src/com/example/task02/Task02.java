f6861ca19fadd0b9de41cc22bbd82a5919dbca23
Wither87 <wither_87@mail.ru>
2021-02-25 20:04:55 +0500

    Крутолевич Сергей Задача 2.2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,9 +4,25 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long num;
+        try {
+            num = Long.parseLong(input);
+        }
+        catch (Exception e) {
+            return "error";
+        }
+        if (num >= Byte.MIN_VALUE && num <= Byte.MAX_VALUE){
+            return "byte";
+        }
+        else if (num >= Short.MIN_VALUE && num <= Short.MAX_VALUE){
+            return "short";
+        }
+        else if (num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE){
+            return "int";
+        }
+        else {
+            return "long";
+        }
     }
 
     public static void main(String[] args) {
