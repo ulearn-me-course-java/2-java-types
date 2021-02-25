921a5f16d2191573bb00246fe779346ff062174b
Anna228322 <anna.fittsdzherald@mail.ru>
2021-02-25 19:43:39 +0500

    ALL TASKS

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -5,13 +5,13 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         int sum = x + y;
-        int count = 0;
+        int co = 0;
         sum = Math.abs(sum);
         while (sum > 0){
             sum /= 10;
-            count++;
+            co++;
         }
-        return count;
+        return co;
     }
 
     public static void main(String[] args) {
