8ce5b4d150faa52c6f9df705646cda98cf8e2140
crutchM <sads1100@gmail.com>
2021-02-25 19:34:41 +0500

    task02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,6 +3,7 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+
         try {
             Byte.parseByte(input);
             return "byte";
