3ad4b46744914a4d3765725a51d76e5cfc19f3d3
Anna228322 <anna.fittsdzherald@mail.ru>
2021-02-25 18:54:03 +0500

    TASK6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,9 +4,14 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = x + y;
+        int count = 0;
+        sum = Math.abs(sum);
+        while (sum > 0){
+            sum /= 10;
+            count++;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
