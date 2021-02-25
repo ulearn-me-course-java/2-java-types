00652c5dc8b1835bae28d4718d2dca8c6745ff93
пк <nikitos.wie@mail.ru>
2021-02-25 11:01:13 +0300

    Яхин Никита - Task_11

--- task11/src/com/example/task11/Task11.java
+++ task11/src/com/example/task11/Task11.java
@@ -5,7 +5,7 @@ import com.sun.imageio.plugins.jpeg.JPEGImageReaderSpi;
 public class Task11 {
 
     public static float benefit(float sum, float percent) {
-        return (float) (Math.pow(1.0+percent,12)*sum);
+        return (float) (Math.pow(1.0 + percent, 12) * sum);
     }
 
     public static void main(String[] args) {
