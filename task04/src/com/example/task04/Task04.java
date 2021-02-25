366724faade5c1c9d510bf133add0e14f5686487
fiepa <fiepaz03@gmail.com>
2021-02-25 11:56:12 +0500

    Task 4

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -3,7 +3,6 @@ package com.example.task04;
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
-
         switch (operation) {
             case ("+"):
                 return a + b;
