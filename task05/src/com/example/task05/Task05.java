0e55ac6ad4a1cfd3f6295f81be05a72f8f11ca3d
RemAdmin <pl712993@gmail.com>
2021-03-02 14:57:37 +0500

    Task_5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -1,21 +1,17 @@
 package com.example.task05;
 
 public class Task05 {
-
+    //normal
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        if (x%2 == 0){
+            return "TRUE";
+        }
+        else return "FALSE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
 
 }
