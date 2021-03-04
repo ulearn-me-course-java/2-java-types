54482e6d7c1c94a4dcc3e36175ebcf339e5ef91a
пк <nikitos.wie@mail.ru>
2021-03-04 14:15:40 +0300

    Яхин - Task_2.14

--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -11,12 +11,7 @@ public class Task14 {
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = reverse(345);
-        System.out.println(result);
-         */
+
     }
 
 
