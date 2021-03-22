40c7d0277ca42b7a0890a8c6fd1da413374661c7
sashapetrova28 <sasha_281200@mail.ru>
2021-03-23 00:53:22 +0500

    Петрова Александра, все задания модуля 2

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -4,10 +4,19 @@ public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if (operation.equals("/"))
+            return (float) a / b;
+        else if (operation.equals("*"))
+            return a * b;
+        else if (operation.equals("+"))
+            return a + b;
+        else if (operation.equals("-"))
+            return a - b;
 
         return 0;
+
     }
+
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,25 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int z = x + y;
+        int count = 0;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if(z == 0)
+            count = 1;
+        else count = 0;
 
-        return 0;
+        while (z != 0) {
+            count++;
+            z = z / 10;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+        int result = solution(1, 567);
         System.out.println(result);
-        */
     }
 
 }
