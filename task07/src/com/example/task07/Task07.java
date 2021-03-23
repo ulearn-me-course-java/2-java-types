e876405d529450344e1238439c7dcd8c308285d0
sashapetrova28 <sasha_281200@mail.ru>
2021-03-23 13:43:55 +0500

    Петрова Александра 2.7

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -4,9 +4,9 @@ public class Task07 {
 
     public static int solution(int n, int m, int k) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int N = n / k;
+        int M = m / k;
+        return N * M;
     }
 
     public static void main(String[] args) {
