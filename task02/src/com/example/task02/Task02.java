a32e9c001ea344c6e82ecec06f745a14356a5c09
sashapetrova28 <sasha_281200@mail.ru>
2021-03-23 12:24:32 +0500

    Петрова Александра 2.2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -7,12 +7,12 @@ public class Task02 {
         double num = Double.parseDouble(input);
         if (num >= Byte.MIN_VALUE && num <= Byte.MAX_VALUE)
             return "byte";
-        else if (num >= Short.MIN_VALUE && num <= Short.MAX_VALUE)
-            return "short";
-        else if (num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE)
-            return "int";
-        else if ( num >= Long.MIN_VALUE && num <= Long.MAX_VALUE)
-            return "long";
+            else if (num >= Short.MIN_VALUE && num <= Short.MAX_VALUE)
+                return "short";
+                else if (num >= Integer.MIN_VALUE && num <= Integer.MAX_VALUE)
+                    return "int";
+                    else if ( num >= Long.MIN_VALUE && num <= Long.MAX_VALUE)
+                    return "long";
         return "";
     }
 
