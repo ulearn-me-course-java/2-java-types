447a292cf710f19cc13d8814d259f51261854007
sashapetrova28 <sasha_281200@mail.ru>
2021-03-23 13:41:34 +0500

    Петрова Александра 2.6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -4,18 +4,26 @@ public class Task06 {
 
     public static int solution(int x, int y) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int z = x + y;
+        int count = 0;
 
-        return 0;
+        if(z == 0)
+            count = 1;
+        else count = 0;
+
+
+        while (z != 0) {
+            count++;
+            z = z / 10;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
