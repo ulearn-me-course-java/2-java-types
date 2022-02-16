6a51bc6eff4e0d7c785c178c6db803d23da6bf12
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-02-17 01:06:57 +0500

    Add files via upload
    Альтернативная задача версия 2.0

---
+++ alternativeTask/src/alternativeTask.java
@@ -0,0 +1,11 @@
+public class alternativeTask {
+    public static void main(String[] args)
+    {
+        /**
+         * 98 + (76 + 5 + 4) / 32 = 100, так как результат храняется в int, только
+         * целая часть нас интересует 76 + 5 + 4 / 32 = 2(2,53125)
+         * И 98 + 2 = 100 :-)
+         */
+        System.out.println(98+(76+5+4)/32);
+    }
+}
