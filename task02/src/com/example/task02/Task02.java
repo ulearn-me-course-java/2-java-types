1a191a403f262bc348f0addff7d899cd092d5b93
PryseOlg <oleg.kuts.02@mail.ru>
2022-02-21 19:32:35 +0500

    Complete Task02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,18 +4,27 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        long digit;
+        try {
+            digit = Long.parseLong(input);
+        } catch (Exception e) {
+            return "Недопустимое значение";
+        }
 
-        return "";
+        if (digit <= Byte.MAX_VALUE && digit >= Byte.MIN_VALUE)
+            return "byte";
+        if (digit <= Short.MAX_VALUE && digit >= Short.MIN_VALUE)
+            return "short";
+        if (digit <= Integer.MAX_VALUE && digit >= Integer.MIN_VALUE)
+            return "int";
+        else return "long";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution("12345");
+        String result = solution("Meow <3");
         System.out.println(result);
-         */
     }
 
 }
