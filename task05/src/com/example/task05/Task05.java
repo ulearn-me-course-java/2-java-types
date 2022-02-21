f09366be27d75c7959dcbabfd464e90b29a7d891
nodlya <nadya-popova-02@mail.ru>
2022-02-21 21:03:28 +0500

    task 2.5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,25 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
+        int xLength = 0;
+        int evenCount = 0;
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        for (;x>0;x/=10) {
+            xLength++;
+            if (x%2 == 0)
+                evenCount++;
+        }
+        if (xLength != evenCount) return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution(1234);
+
+        String result = solution(264);
         System.out.println(result);
-        */
+
     }
 
 }
