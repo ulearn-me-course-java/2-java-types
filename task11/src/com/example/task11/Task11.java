90862db411595f560cabf64d8080128dc824638a
IlyaIIS <ilya1221221@mail.ru>
2022-02-21 19:12:09 +0500

    Исмаков Илья - task 2.11

--- task11/src/com/example/task11/Task11.java
+++ task11/src/com/example/task11/Task11.java
@@ -11,7 +11,6 @@ public class Task11 {
         float percent = 0.00000001f; // 0.000001% ежемесячно
 
         sum = benefit(sum, percent);
-
         System.out.println("Сумма на счете через год: " + sum);
     }
 
