58f34d67e8b4fd54c2c4cf115a466dcb08b87ee8
PryseOlg <oleg.kuts.02@mail.ru>
2022-02-21 21:46:15 +0500

    Complete Task06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,19 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        return countOfDigit(x+y);
+    }
+    public static int countOfDigit(int sum){
+        if (String.valueOf(sum).contains("-"))
+            return (String.valueOf(sum).length() - 1);
+        else return String.valueOf(sum).length();
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
