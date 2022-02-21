fe29fd7b76c9010bc8d8b859e988d119dc26fc6b
nodlya <nadya-popova-02@mail.ru>
2022-02-21 21:12:52 +0500

    task 2.6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -5,17 +5,20 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int z = x+y;
+        if (z == 0) return 1;
+        int zLength = 0;
+        for (;Math.abs(z)>0;z/=10) zLength++;
+        return zLength;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
