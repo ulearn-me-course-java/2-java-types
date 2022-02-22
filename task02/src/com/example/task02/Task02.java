3d627eaa59b951b80dcdfdedc8f46a1f676b6e27
WEBMaximWEB <maxim.geer@yandex.ru>
2022-02-23 01:47:43 +0500

    Геер Максим. task02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,10 +3,21 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        //input = input.replace("-", "");
+        long num = Long.parseLong(input);
+        if(num >= -128 && num <= 127)
+        {
+            return "byte";
+        }
+        else if(num >= -32768 && num <= 32767)
+        {
+            return "short";
+        }
+        else if(num >= -2147483648 && num <= 2147483647)
+        {
+            return "int";
+        }
+        return "long";
     }
 
     public static void main(String[] args) {
