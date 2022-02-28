d90c008b5c3fec4703d197b17c6d5cc367f9f343
ArsaGit <artyom.arsalanov@gmail.com>
2022-02-28 19:12:17 +0500

    Арсаланов Артём. Задание 2.2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,10 +3,20 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        long number = Long.parseLong(input);
+        String result;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        if (isInRange(number, Byte.MIN_VALUE, Byte.MAX_VALUE)) result = "byte";
+        else if (isInRange(number, Short.MIN_VALUE, Short.MAX_VALUE)) result = "short";
+        else if (isInRange(number, Integer.MIN_VALUE, Integer.MAX_VALUE)) result = "int";
+        else if (isInRange(number, Long.MIN_VALUE, Long.MAX_VALUE)) result = "long";
+        else result = "error";
 
-        return "";
+        return result;
+    }
+
+    public static boolean isInRange(long number, long min, long max) {
+        return number >= min && number <= max;
     }
 
     public static void main(String[] args) {
