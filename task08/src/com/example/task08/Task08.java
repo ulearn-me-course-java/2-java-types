552da8fcaacdf04f8b283feb4d9c1eaa69637771
Fofan <vovka.322@Mail.ru>
2022-03-02 01:08:50 +0600

    Ефимов Иван Задание 08

--- task08/src/com/example/task08/Task08.java
+++ task08/src/com/example/task08/Task08.java
@@ -5,6 +5,7 @@ public class Task08 {
     public static boolean solution() {
         float x = Float.MAX_VALUE;
         return x == x + 1;
+        //
     }
 
     public static void main(String[] args) {
