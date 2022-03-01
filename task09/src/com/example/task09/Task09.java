3fd9a6bfb707fb1884f1ddfb6025f1c7cbd2f51c
Saveliy <savelussava@mail.ru>
2022-03-01 20:19:28 +0500

    Кудрявцев Савелий

--- task09/src/com/example/task09/Task09.java
+++ task09/src/com/example/task09/Task09.java
@@ -7,16 +7,20 @@ public class Task09 {
 
         float a = 1.0f;
         float b = 3.0f;
-        double x = (a / b - 1.0 / 3.0) * 1.0e9;
+
+        double x = a / b * 1.0e9 - 1.0 / 3.0 * 1.0e9;
+
+        if(a / b - 1.0 / 3.0 <10e-9){
+            return 0;
+        }
 
         return x;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение
-        /*
+
         System.out.println(solution());
-        */
+
     }
 
 }
