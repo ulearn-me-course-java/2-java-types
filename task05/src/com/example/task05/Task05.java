93ef9064f5ef346e73036bb8e513ba84b766a72b
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-03-01 23:40:45 +0500

    Task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -6,7 +6,9 @@ public class Task05 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "FALSE";
+        for (;x > 0; x/= 10)
+            if (x % 2 == 1) return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
