54c32cc7334bce25b8d45989d527290492a6d32c
DemLoveSky <dem.loginov@gmail.com>
2022-03-02 05:57:49 +0500

    Add code
    Логинов Дмитрий. Задание 2.6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,19 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int sum = Math.abs(x + y);
+        int count = 0;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        if (sum == 0) return 1;
+        for (int i = 1; i <= sum; i *= 10) {
+            count++;
+        }
+        return count;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
