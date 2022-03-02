ba99e379a3cfc1ecad5716e5e8e6fe7cf5828168
DemLoveSky <dem.loginov@gmail.com>
2022-03-02 05:06:45 +0500

    Add code
    Логинов Дмитрий. Задание 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,18 +4,23 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        Long number;
+        try {
+            number = Long.valueOf(input);
+        } catch (NumberFormatException longErr) {
+            return "";
+        }
 
-        return "";
+        if (Byte.MIN_VALUE <= number && Byte.MAX_VALUE >= number) return "byte";
+        else if (Short.MIN_VALUE <= number && Short.MAX_VALUE >= number) return "short";
+        else if (Integer.MIN_VALUE <= number && Integer.MAX_VALUE >= number) return "int";
+        else return "long";
+
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
