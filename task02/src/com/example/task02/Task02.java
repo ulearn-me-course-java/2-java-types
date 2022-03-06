2b4d0a5cc7e02cf861269e149a8e7c2e67f6c65a
gorosheek <yulyagroshenko@mail.ru>
2022-03-06 22:00:31 +0300

    Грошенко Юлия

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -5,11 +5,11 @@ public class Task02 {
     public static String solution(String input) {
 
         long var = Long.parseLong(input);
-        if (var < Byte.MAX_VALUE && var > Byte.MIN_VALUE)
+        if (var <= Byte.MAX_VALUE && var >= Byte.MIN_VALUE)
             return "byte";
-        else if (var < Short.MAX_VALUE && var > Short.MIN_VALUE)
+        else if (var <= Short.MAX_VALUE && var >= Short.MIN_VALUE)
             return "short";
-        else if (var < Integer.MAX_VALUE && var > Integer.MIN_VALUE)
+        else if (var <= Integer.MAX_VALUE && var >= Integer.MIN_VALUE)
             return "int";
         else
             return "long";
@@ -18,7 +18,8 @@ public class Task02 {
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-
+        String result = solution("127");
+        System.out.println(result);
 
 
     }
