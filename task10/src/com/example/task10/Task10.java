cf4a51f92dd89a0412bca4ab2c4b59153a16ec62
vlaD <vlad.74022@gmail.com>
2022-03-13 21:39:45 +0500

    Task 2.10 Рытый Владислав, ПрИ-201

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -7,8 +7,16 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
+        if (Float.isNaN((a)) && Float.isNaN((b)))
+            return true;
+
+        else if (Float.isInfinite(a) && Float.isInfinite(b) && !Float.isNaN(a + b))
+            return true;
+
+        else if (precision == 0)
+            return a == b;
+
+        return Math.abs(Math.abs(a) - Math.abs(b)) <= Math.pow(0.1f, precision);
 
     }
 
