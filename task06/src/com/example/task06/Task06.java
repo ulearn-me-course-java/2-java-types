56abdb803d9febc0cdb22fef8e7cca302e6c9909
Degener8 <raptoroidinbox@gmail.com>
2022-03-15 21:35:25 +0500

    task06 Гриценюк ПрИ-201

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -2,20 +2,25 @@ package com.example.task06;
 
 public class Task06 {
 
-    public static int solution(int x, int y) {
+    public static int solution(int x, int y)
+    {
+        int sum = Math.abs(x + y);
+        if(sum == 0)
+            return 1;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int res = 0;
+        while(sum > 0)
+        {
+            sum /= 10;
+            res ++;
+        }
 
-        return 0;
+        return res;
     }
 
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(12, 34);
-        System.out.println(result);
-        */
+    public static void main(String[] args)
+    {
+        System.out.print(solution(-5512,-35046));
     }
 
 }
