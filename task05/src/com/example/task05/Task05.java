315750553745089f3db45299898150374d0e8573
Degener8 <raptoroidinbox@gmail.com>
2022-03-15 21:22:48 +0500

    task05 Гриценюк ПрИ-201

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -2,20 +2,22 @@ package com.example.task05;
 
 public class Task05 {
 
-    public static String solution(int x) {
+    public static String solution(int x)
+    {
+        while(x > 0)
+        {
+            if(x % 2 != 0)
+                return "FALSE";
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+            x /= 10;
+        }
 
-        return "FALSE";
+        return "TRUE";
     }
 
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
+    public static void main(String[] args)
+    {
+        System.out.println(solution(5202));
     }
 
 }
