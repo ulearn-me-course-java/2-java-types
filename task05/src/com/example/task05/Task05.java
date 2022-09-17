c451da702c4d9c927621edc08e4171262fdfbf5b
EVCHEPUCCYA <beerklaro@bk.ru>
2022-09-18 00:30:41 +0500

    Лутков Евгений task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,29 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        int evenNumb = 0;
+        int i = 0;
+        while (x != 0) {
+            if ((x % 10) % 2 == 0) {
+                evenNumb++;
+            }
+            x /= 10;
+            i++;
+        }
+        if (evenNumb == i) {
+            return "TRUE";
+        } else {
+            return "FALSE";
+        }
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
