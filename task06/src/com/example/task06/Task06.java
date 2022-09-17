ae0b3c0a4a58e782b100860cff168456083f736d
EVCHEPUCCYA <beerklaro@bk.ru>
2022-09-18 00:19:02 +0500

    Лутков Евгений task06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,10 +3,8 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = Math.abs(x+y);
+        return (String.valueOf(sum).length());
     }
 
     public static void main(String[] args) {
