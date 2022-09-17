8c60a19f066d02f6d556e4d2e68e38a06ee6a962
Arseny <20mineslend14@gmail.com>
2022-09-17 18:49:28 +0500

    task10

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -1,5 +1,7 @@
 package com.example.task10;
 
+import java.sql.PreparedStatement;
+
 public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
@@ -7,8 +9,21 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
+        if (Float.isNaN(a) && Float.isNaN(b)){
+            return true;
+        } else if (Float.isNaN(a) | Float.isNaN(b)) {
+            return false;
+        } else if (Float.isInfinite(a) && Float.isInfinite(b) && !Float.isNaN(a + b)) {
+            return true;
+        }
+        if (precision == 0){
+            return a == b;
+        }
+        long pow = 1;
+        for (int i = 0; i < precision; ++i){
+            pow *= 10;
+        }
+        return Math.floor(a * pow) == Math.floor(b * pow);
 
     }
 
@@ -18,7 +33,7 @@ public class Task10 {
         float sum = a + b;
         float c = 0.7f;
 
-        boolean result = compare(sum, c, 2);
+        boolean result = compare(1.f, 2.f, 10);
         System.out.println(result);
 
     }
