1c1a46b02db036cfadfffd990a19ffc2a8399a64
by_Onyx <ilyayaldinov@inbox.ru>
2022-09-17 21:36:39 +0500

    task5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,10 +3,14 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        for (int i = 0; i < 5; i++){
+            if((x % 10) % 2 != 0){
+                return "FALSE";
+            }
+            x = x / 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
