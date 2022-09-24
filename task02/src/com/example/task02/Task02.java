4e846b1efcb733f278d384e46f31564920e487cc
Ivan Danilyuk <werwetsad@gmail.com>
2022-09-24 23:58:28 +0500

    Completed Task 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,40 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        try {
+            byte num = Byte.parseByte(input);
+            return "Byte";
+        }
+        catch (Exception ex)
+        {
+        }
+        try {
+            short num = Short.parseShort(input);
+            return "Short";
+        }
+        catch (Exception ex)
+        {
+        }
+        try {
+            int num = Integer.parseInt(input);
+            return "Integer";
+        }
+        catch (Exception ex)
+        {
+        }
+        try {
+            long num = Long.parseLong(input);
+            return "Long";
+        }
+        catch (Exception ex)
+        {
+        }
+        return "Long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
