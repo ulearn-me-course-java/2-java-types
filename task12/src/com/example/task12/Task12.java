cd0dbc709173da2f7ab681864491788f1d786c52
Rumyancev-Egor <egor.rumyancev.9494@mail.ru>
2022-09-25 02:57:40 +0500

    Румянцев Егор task12

--- task12/src/com/example/task12/Task12.java
+++ task12/src/com/example/task12/Task12.java
@@ -8,16 +8,10 @@ public class Task12 {
 
         // TODO раскомментируйте и исправьте код
 
-        // Считаем проценты за год
+        for (int i = 1; i <= 12; i++)
+            sum = sum.add(sum.multiply(percent));
 
-        /*
-        for (int i = 1; i <= 12; i++) {
-            sum += sum * percent;
-        }
-        return sum;
-        */
-
-        return BigDecimal.ZERO;
+        return sum.setScale(9, BigDecimal.ROUND_HALF_UP);
     }
 
     public static void main(String[] args) {
