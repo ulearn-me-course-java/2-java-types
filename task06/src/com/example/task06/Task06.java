a79e8c27fae90126d8ff1554994e343790f1aac4
Veronika Dyminskaya <veroni4ka_d03@mail.ru>
2022-09-26 02:40:59 +0500

    Task_6

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -5,17 +5,17 @@ public class Task06 {
     public static int solution(int x, int y) {
        x+=y;
        String str = Integer.toString(x);
-
+       if(str.contains("-")){
+           return str.length()-1;
+       }
         return str.length();
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
