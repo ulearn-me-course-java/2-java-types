865c8b1ec231d4b7033b7af7032501e7ecbed01e
Veronika Dyminskaya <veroni4ka_d03@mail.ru>
2022-09-26 01:55:13 +0500

    Task_5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,29 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+       if (x>0) {
+           x/=10;
+           if (x>0) {
+               x/=10;
+               if (x>0) {
+                   x/=10;
+                   if (x>0) {
+                       x/=10;
+                       if (x==0) {
+                           return  "True";
+                       }
+                   }
+               }
+           }
+       }
+       return "FALSE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
