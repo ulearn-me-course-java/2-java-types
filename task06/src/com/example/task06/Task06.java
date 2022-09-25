b5074071c76a0ebcead1a71ffc98d87d5bbc4e2f
Vlada <ogarkova.vladislava@mail.ru>
2022-09-25 23:21:40 +0500

    Огаркова В. ПрИ-202

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,23 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int count = 0;
+        int res = x + y;
+        if(res != 0){
+            while(res != 0){
+                res /= 10;
+                count++;
+            }
+            return count;
+        }
+        else return 1;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int result = solution(12, 34);
         System.out.println(result);
-        */
+
     }
 
 }
