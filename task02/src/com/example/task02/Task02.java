f8341f9a9ba18b5b670cb0c98d7143d92a56ee49
Veronika Dyminskaya <veroni4ka_d03@mail.ru>
2022-09-26 01:26:42 +0500

    Task_2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,18 +4,32 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long lg = Long.parseLong(input);
+        int in = (int) lg;
+        if(in==lg)
+        {
+            short sh =(short) in;
+            if(sh == in) {
+                byte by = (byte) sh;
+                if(by == sh) {
+                    return "byte";
+                }
+                else {
+                    return "short";
+                }
+            }
+            else{
+                return"int";
+            }
+        }
+        return "long";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
