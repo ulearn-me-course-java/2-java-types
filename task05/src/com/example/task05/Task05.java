d24588ef542939e28c8d3c17db26fecec5868d29
Vlada <ogarkova.vladislava@mail.ru>
2022-09-25 23:10:22 +0500

    Огаркова В. ПрИ-202

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -4,18 +4,20 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        for (int n = 0; n < 5; n++){
 
-        return "FALSE";
+            if((x % 10) % 2 != 0) return "FALSE";
+
+            x = x / 10;
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
