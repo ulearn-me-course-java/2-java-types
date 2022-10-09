e50ec9047e7ca42df0252afce612fbb5ef1bb811
nbespalovv <Dnevnik1400@mail.ru>
2022-10-09 18:10:26 +0500

    Задание 4.

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -5,8 +5,10 @@ public class Task04 {
     public static float calculate(int a, int b, String operation) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        if (operation.equals("+")) return a+b;
+        if (operation.equals("-")) return  a-b;
+        if (operation.equals("/")) return (float) a/b;
+        return a*b;
     }
 
     public static void main(String[] args) {
