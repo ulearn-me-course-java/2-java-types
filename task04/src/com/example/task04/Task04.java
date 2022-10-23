03feb6c288479ebe2e7d29315fabe95b93110edb
Olga <dikay.pech@mail.ru>
2022-10-23 19:51:39 +0300

    Мотовилова ПрИ-201 Модуль 2

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -4,18 +4,26 @@ public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        float num1 = (float)a;
+        float num2 = (float)b;
+        if(operation == "+")
+            return num1 + num2;
+        else if(operation == "-")
+            return num1 - num2;
+        else if(operation == "*")
+            return num1 * num2;
+        else
+            return num1 / num2;
 
-        return 0;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -6,17 +6,19 @@ public class Task14 {
     public static int reverse(int value) {
 
         // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
+        String str = String.valueOf(value);
+        str = new StringBuilder(str).reverse().toString();
 
-        return 0;
+        return Integer.parseInt(str);
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int result = reverse(345);
         System.out.println(result);
-         */
+
     }
 
 
