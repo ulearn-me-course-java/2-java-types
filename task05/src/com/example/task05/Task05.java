4393978d44f4e483221183e40940851a60faec32
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-09-12 08:48:42 +0500

    fifth

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -4,18 +4,17 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        String str = Integer.toString(x);
+        for (char symbol:str.toCharArray()) {
 
-        return "FALSE";
+            if((symbol-'0')%2==1) return "FALSE";
+
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution(1234);
-        System.out.println(result);
-        */
+
     }
 
 }
