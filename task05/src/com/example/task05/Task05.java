011cf7626d5f58dd87a0da6a9cefda2a4ea7e792
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-09-12 21:47:51 +0500

    Finished task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,21 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
+        while (x > 0) {
+            int digit = x % 10;
+            x /= 10;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+            if (digit % 2 != 0) {
+                return "FALSE";
+            }
+        }
 
-        return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
 
 }
