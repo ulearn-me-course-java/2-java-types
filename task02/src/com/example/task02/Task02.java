7b3942d5ea3146050235f3c842211460ed08962b
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-09-12 21:36:55 +0500

    Finished task02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,32 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        try {
+            byte n = Byte.parseByte(input);
+            return "byte";
+        } catch (NumberFormatException e) {  }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        try {
+            short n = Short.parseShort(input);
+            return "short";
+        } catch (NumberFormatException e) {  }
+
+        try {
+            int n = Integer.parseInt(input);
+            return "int";
+        } catch (NumberFormatException e) {  }
+
+        try {
+            long n = Long.parseLong(input);
+            return "long";
+        } catch (NumberFormatException e) {  }
 
         return "";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
