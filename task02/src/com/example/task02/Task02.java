d3ecfb806ebf8aff5bcc9dffb35ef4d99717a959
Степан <@LAPTOP-B0PF4KGO>
2023-09-13 17:19:08 +0500

    Бойко Степан. Все задания

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,20 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long inputAsNumber = Long.parseLong(input);
+        if (inputAsNumber <= Byte.MAX_VALUE && inputAsNumber >= Byte.MIN_VALUE) {
+            return "byte";
+        } else if (inputAsNumber <= Short.MAX_VALUE && inputAsNumber >= Short.MIN_VALUE) {
+            return "short";
+        } else if (inputAsNumber <= Integer.MAX_VALUE && inputAsNumber >= Integer.MIN_VALUE) {
+            return "int";
+        }
+        return "long";
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
