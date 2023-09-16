d762ca55d0facd76bcae56ebeed0ec90d4b9fb99
Ekaterina Fedotova <katyushka_musa@mail.ru>
2023-09-16 22:57:40 +0500

    Федотова Екатерина. Задание 10

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -1,26 +1,20 @@
 package com.example.task10;
+import jdk.internal.math.FloatingDecimal;
+
+import java.lang.Math;
 
 public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
+        if (Float.isNaN(a) && Float.isNaN(b)) return true;
+        else if (Float.isNaN(a) || Float.isNaN(b)) return false;
+        else if (Float.isInfinite(a) && Float.isInfinite(b) && !Float.isNaN(a + b)) return true;
 
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
-
+        float multiplier = (float) Math.pow(10, precision);
+        return (long) (a * multiplier) == (long) (b * multiplier);
     }
 
     public static void main(String[] args) {
-        float a = 0.3f;
-        float b = 0.4f;
-        float sum = a + b;
-        float c = 0.7f;
-
-        boolean result = compare(sum, c, 2);
-        System.out.println(result);
-
     }
 
 }
