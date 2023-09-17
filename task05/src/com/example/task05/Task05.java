2ef1cb6a8c5fe862d1bb425e4af16f68a1e43a4f
pe4enka <girfanov_kostia@mail.ru>
2023-09-17 22:43:30 +0500

    task5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -1,12 +1,19 @@
 package com.example.task05;
+import java.io.*;
 
 public class Task05 {
 
     public static String solution(int x) {
+        String value = Integer.toString(x);
+        for(int i = 0; i < value.length(); i++)
+        {
+            char[] array = value.toCharArray();
+            if(Integer.parseInt(String.valueOf(array[i])) % 2 != 0){
+                return "FALSE";
+            }
+        }
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
