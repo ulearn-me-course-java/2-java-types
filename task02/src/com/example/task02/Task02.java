cebc0eec2790a99be6cead90600159e44d2428cf
polin <89823206706@mail.ru>
2023-09-23 01:06:01 +0300

    task2

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -6,16 +6,37 @@ public class Task02 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "";
+        try {
+            Byte.parseByte(input);
+            return "byte";
+        }
+        catch (Exception ignored){
+        }
+
+        try {
+            Short.parseShort(input);
+            return "short";
+        }
+        catch (Exception ignored){
+        }
+
+        try {
+            Integer.parseInt(input);
+            return "int";
+        }
+        catch (Exception ignored){
+        }
+
+        return "long";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution("12345");
+
+        String result = solution("24234242222");
         System.out.println(result);
-         */
+
     }
 
 }
