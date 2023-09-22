85b5eb336150fda78f208f449415051a200e280d
polin <89823206706@mail.ru>
2023-09-23 01:06:19 +0300

    task4

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -1,21 +1,32 @@
 package com.example.task04;
 
+import java.util.Objects;
+
 public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+        if (Objects.equals(operation, "+")) {
+            return (float) a + b;
+        }
+        if (Objects.equals(operation, "-")) {
+            return (float) a - b;
+        }
+        if (Objects.equals(operation, "*")) {
+            return (float) a * b;
+        }
+        return (float) a / b;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
