74b330b8f2f2b4f9bf8b5ecbf1631452922b708a
LeeLuiza <116955531+LeeLuiza@users.noreply.github.com>
2023-09-22 23:22:51 +0200

    laba2

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,19 +3,21 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
+        int sum = x + y;
+        int numberOfDigits = 0;
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        while (sum != 0) {
+            sum /= 10;
+            numberOfDigits++;
+        }
+        if (numberOfDigits == 0)
+            numberOfDigits++;
+        return numberOfDigits;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int result = solution(12, 34);
         System.out.println(result);
-        */
     }
 
 }
