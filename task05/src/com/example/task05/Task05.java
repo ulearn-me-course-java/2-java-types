2a9686dd8e2d47c3279f9500b806d000211725e1
polin <89823206706@mail.ru>
2023-09-23 01:06:30 +0300

    task5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -6,16 +6,23 @@ public class Task05 {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "FALSE";
+        boolean result = false;
+        char[] number = String.valueOf(x).toCharArray();
+        for (char i :
+                number) {
+            if (Integer.parseInt(String.valueOf(i)) % 2 == 0) continue;
+            result = true;
+        }
+        return result ? "FALSE" : "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         String result = solution(1234);
         System.out.println(result);
-        */
+
     }
 
 }
