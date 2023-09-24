c6c8b194a960d732726d025d09622686e3e07665
Elizaveta Pernikova <lisapernikowa@mail.ru>
2023-09-24 19:04:42 +0500

    tasks 2.01 - 2.14

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,19 +3,23 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long inputNum = Long.parseLong(input);
+        if(inputNum <= Byte.MAX_VALUE && inputNum >= Byte.MIN_VALUE)
+            return "byte";
+        else if(inputNum <= Short.MAX_VALUE && inputNum >= Short.MIN_VALUE)
+            return "short";
+        else if(inputNum <= Integer.MAX_VALUE && inputNum >= Integer.MIN_VALUE)
+            return "int";
+        return "long";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         String result = solution("12345");
         System.out.println(result);
-         */
+
     }
 
 }
--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,10 +3,11 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        String str = String.valueOf(x);
+        for (int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) % 2 != 0) return "FALSE";
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
