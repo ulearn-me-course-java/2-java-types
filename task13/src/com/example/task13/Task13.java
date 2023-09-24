9a73031216834dd20f83ccae3b9146a603f53e72
Diana <lici4qweru090@gmail.com>
2023-09-25 00:01:00 +0500

    task13 is done

--- task13/src/com/example/task13/Task13.java
+++ task13/src/com/example/task13/Task13.java
@@ -3,19 +3,13 @@ package com.example.task13;
 public class Task13 {
 
     public static char toUpperCase(char c) {
-
-        // TODO привести букву к верхнему регистру
-
-        return c;
+        char res = (char) (c - 32);
+        return res;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        char result = solution('x');
+        char result = toUpperCase('a');
         System.out.println(result);
-         */
     }
 
 }
