d811239e114187be7b858694100e53b32b1f0c8f
Igor <IgorLom2003@mail.ru>
2023-10-09 01:15:48 +0500

    Ломовцев Игорь ПрИ-201

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -3,10 +3,8 @@ package com.example.task06;
 public class Task06 {
 
     public static int solution(int x, int y) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int sum = x+y;
+        return Integer.toString(Math.abs(sum)).length();
     }
 
     public static void main(String[] args) {
