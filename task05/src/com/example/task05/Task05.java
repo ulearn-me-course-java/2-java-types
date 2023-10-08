cc4e4b38fc51deb99dc62b3df84090783f1907ae
LoiLok <artemklpkv@gmail.com>
2023-10-08 21:26:44 +0500

    task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -3,19 +3,19 @@ package com.example.task05;
 public class Task05 {
 
     public static String solution(int x) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        String num = Integer.toString(x);
+        for (int i = 0; i < num.length(); i++) {
+            if (num.charAt(i) % 2 != 0) {
+                return "FALSE";
+            }
+        }
+        return "TRUE";
     }
 
+
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
 
 }
