d0205b84fe3d89bc37311feec933f84e6cfffb2d
Tiffecko <113545856+Tiffecko@users.noreply.github.com>
2023-10-09 10:44:52 +0500

    T10 fix

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -3,20 +3,15 @@ package com.example.task10;
 public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
-        if (Float.isNaN(a) || Float.isNaN(b)) {
-            return Float.isNaN(a) && Float.isNaN(b);
-        } else {
-            if (a > 0 && b > 0) {
-                if (Float.isInfinite(a) && Float.isInfinite(b)) {
-                    return true;
-                }
-                return Math.abs(a - b) < Math.pow(10, -precision);
-            } else {
-                if (a >= 0 && b <= 0) {
-                    return false;
-                } else return !(a <= 0) || !(b >= 0);
-            }
+        if (Float.isInfinite(a) && Float.isInfinite(b)) {
+            return a == b;
         }
+
+        if(Float.isNaN(a) && Float.isNaN(b)){
+            return true;
+        }
+
+        return Math.abs(a-b) < Math.pow(0.1, precision);
     }
 
     public static void main(String[] args) {
