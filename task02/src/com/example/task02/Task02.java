4d496930fd0f2ef03652bfc053a21135aba7c556
Pain-Excalibur <ffggh.fn2@gmail.com>
2023-10-23 00:05:38 +0500

    done

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -4,18 +4,29 @@ public class Task02 {
 
     public static String solution(String input) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        try {
+            Byte.parseByte(input);
+            return "byte";
+        } catch (NumberFormatException e) {}
 
-        return "";
+        try {
+            Short.parseShort(input);
+            return "short";
+        } catch (NumberFormatException e) {}
+
+        try {
+            Integer.parseInt(input);
+            return "int";
+        } catch (NumberFormatException e) {}
+
+        return "long";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         String result = solution("12345");
         System.out.println(result);
-         */
     }
 
 }
--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -4,18 +4,27 @@ public class Task04 {
 
     public static float calculate(int a, int b, String operation) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        float bFloat = b;
+        switch (operation)
+        {
+            case "+":
+                return a + bFloat;
+            case "-":
+                return a - bFloat;
+            case "/":
+                return a / bFloat;
+            case "*":
+                return a * bFloat;
+            default:
+                return 69;
+        }
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
     }
 
 }
