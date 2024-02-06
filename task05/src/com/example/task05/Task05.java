ac65306fde5d673b2d9b66a5b2a8ce0edcf6bee2
D00M33D <al3ksey.19@gmail.com>
2024-02-06 20:58:20 +0500

    Completed tasks

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -5,8 +5,22 @@ public class Task05 {
     public static String solution(int x) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        boolean flag = true;
 
-        return "FALSE";
+        while (x > 0) {
+            if (x % 2 != 0){
+                flag = false;
+                x /= 10;
+            } else {
+                x /= 10;
+            }
+        }
+
+        if (flag == true) {
+            return "TRUE";
+        }
+        else {
+        return "FALSE"; }
     }
 
     public static void main(String[] args) {
--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -3,10 +3,12 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return 0;
+        int newlength = n / k;
+        int newwidth = m / k;
+
+        return newlength * newwidth;
     }
 
     public static void main(String[] args) {
