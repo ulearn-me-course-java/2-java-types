9b6f8673a1f07cf355364515afd08b0cd8a2d0ee
MabusTFC <113986000+MabusTFC@users.noreply.github.com>
2024-02-09 23:17:44 +0500

    Смирнов ИА ПрИ201

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -1,21 +1,24 @@
 package com.example.task05;
-
 public class Task05 {
-
     public static String solution(int x) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        for (int i = 0;i<5;i++){
+            if ((x%10)%2==1){
+                return "FALSE";
+            }
+            x/=10;
+        }
 
-        return "FALSE";
+        return "TRUE";
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution(1234);
+
+        String result = solution(88888);
         System.out.println(result);
-        */
     }
 
 }
