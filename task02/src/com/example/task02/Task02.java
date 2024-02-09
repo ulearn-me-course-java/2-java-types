02651a0281f3d8f4bfa209cd1327800ef61bde74
lenadekart <decartfamily@vk.com>
2024-02-09 21:49:46 +0500

    В метод Task02.solution добавлена логика, позволяющая выполнить наименьший целочисленный тип, к которому можно привести входящее число.

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,10 +3,16 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
-
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
 
-        return "";
+        long num = Long.parseLong(input);
+        if (num <= 127 && num >= -128)
+            return "byte";
+        else if (num <= 32767 && num >= -32768)
+            return "short";
+        else if (num>= -2147483648 && num <= 2147483647)
+            return "int";
+        else return "long";
     }
 
     public static void main(String[] args) {
