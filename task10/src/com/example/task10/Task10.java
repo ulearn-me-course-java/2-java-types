d853036771b1660ea61f1e12fbc8f63dea253826
lenadekart <decartfamily@vk.com>
2024-02-09 22:47:12 +0500

    Теперь метод Task10.compare корректно сравнивает два значения типа float. Присутствует проверка на бесконечные и на "не число".

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -3,13 +3,16 @@ package com.example.task10;
 public class Task10 {
 
     public static boolean compare(float a, float b, int precision) {
+        if (Float.isNaN(a) && Float.isNaN(b)) {
+            return true;
+        }
+        if (Float.isInfinite(a) && Float.isInfinite(b)) {
+            return a == b;
+        }
 
-        // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
-        // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
-        // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
-
+        float difference = Math.abs(a - b);
+        float epsilon = (float) Math.pow(10, -precision);
+        return difference < epsilon;
     }
 
     public static void main(String[] args) {
